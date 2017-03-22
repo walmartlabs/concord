5852811f46dec27138b130b4ac3a654929c1a2c3
jayeshpise <jayesh.pise@gmail.com>
2017-03-22 13:43:05 -0700

    cleanup/changes as per code review comments

--- plugins/tasks/boo/src/main/java/com/walmartlabs/concord/plugins/boo/BooTask.java
+++ plugins/tasks/boo/src/main/java/com/walmartlabs/concord/plugins/boo/BooTask.java
@@ -6,14 +6,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Named;
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
 
 @Named
 public class BooTask implements Task {
     private static final Logger log = LoggerFactory.getLogger(BooTask.class);
-
-    private static final int SUCCESS_EXIT_CODE = 0;
 
     @Override
     public String getKey() {
@@ -23,11 +24,17 @@ public class BooTask implements Task {
     public void run(Map<String, Object> args, String payloadPath) throws Exception {
         log.info("Inside boo task impl. Input: " + args.get("input"));
         BooCli boo = new BooCli();
-        String booTemplatePath = payloadPath + "/" + args.get("booTemplateLocation");
+        //String booTemplatePath = payloadPath + "/" + args.get("booTemplateLocation");
+        Path booTemplatePath = Paths.get(payloadPath).resolve(String.valueOf(args.get("booTemplateLocation")));
+        if (!Files.exists(booTemplatePath)) {
+            // we can't continue
+            throw new IOException("Boo template file not found: " + booTemplatePath.toAbsolutePath());
+        }
+
         Map<String, String> booTemplateVariables = (Map<String, String>) args.get("booTemplateVariables");
         log.info("boo Variables: " + booTemplateVariables);
-        log.info("boo template path: " + booTemplatePath);
-        boo.init(new File(booTemplatePath), null, booTemplateVariables , null);
+        log.info("boo template path: " + booTemplatePath.toAbsolutePath());
+        boo.init(booTemplatePath.toFile(), null, booTemplateVariables , null);
         boo.createPacks(false, false);
     }
 }
