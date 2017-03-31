31e2e00e1f743742ee7538ba86d58ea7de9c3199
Ivan Bodrov <ibodrov@gmail.com>
2017-03-30 22:15:21 -0400

    console: serve landing pages

--- docker-images/console/src/main/docker/start.sh
+++ docker-images/console/src/main/docker/start.sh
@@ -5,5 +5,5 @@ SERVER_PORT="$SERVER_PORT_8001_TCP_PORT"
 
 NGINX_CONF_DIR="/opt/concord/console/nginx/conf"
 
-/usr/bin/sed "s/SERVER_ADDR/$SERVER_ADDR/g;s/SERVER_PORT/$SERVER_PORT/g" $NGINX_CONF_DIR/app.template > $NGINX_CONF_DIR/app.conf
-/usr/sbin/nginx -c $NGINX_CONF_DIR/nginx.conf
+/usr/bin/sed "s/SERVER_ADDR/$SERVER_ADDR/g;s/SERVER_PORT/$SERVER_PORT/g" ${NGINX_CONF_DIR}/app.template > ${NGINX_CONF_DIR}/app.conf
+/usr/sbin/nginx -c ${NGINX_CONF_DIR}/nginx.conf
