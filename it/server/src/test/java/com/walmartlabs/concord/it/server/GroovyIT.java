0cee93d476bba4893f8209be3e3a5c584db3cfbe
Ivan Bodrov <ibodrov@gmail.com>
2017-04-25 14:03:51 -0400

    bump the bpm engine version, add an IT for groovy scripting

---
+++ it/server/src/test/java/com/walmartlabs/concord/it/server/GroovyIT.java
@@ -0,0 +1,50 @@
+package com.walmartlabs.concord.it.server;
+
+import com.walmartlabs.concord.server.api.process.ProcessResource;
+import com.walmartlabs.concord.server.api.process.ProcessStatusResponse;
+import com.walmartlabs.concord.server.api.process.StartProcessResponse;
+import com.walmartlabs.concord.server.api.security.apikey.ApiKeyResource;
+import com.walmartlabs.concord.server.api.security.apikey.CreateApiKeyRequest;
+import com.walmartlabs.concord.server.api.security.apikey.CreateApiKeyResponse;
+import com.walmartlabs.concord.server.api.user.CreateUserRequest;
+import com.walmartlabs.concord.server.api.user.CreateUserResponse;
+import com.walmartlabs.concord.server.api.user.UserResource;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.util.Collections;
+
+import static org.junit.Assert.assertNotNull;
+
+public class GroovyIT extends AbstractServerIT {
+
+    @Test(timeout = 30000)
+    public void test() throws Exception {
+        String username = "user_" + System.currentTimeMillis();
+
+        UserResource userResource = proxy(UserResource.class);
+        CreateUserResponse cur = userResource.createOrUpdate(new CreateUserRequest(username, Collections.singleton("*")));
+
+        ApiKeyResource apiKeyResource = proxy(ApiKeyResource.class);
+        CreateApiKeyResponse cakr = apiKeyResource.create(new CreateApiKeyRequest(cur.getId()));
+
+        setApiKey(cakr.getKey());
+
+        // ---
+
+        byte[] payload = archive(ProcessIT.class.getResource("groovy").toURI(), ITConstants.DEPENDENCIES_DIR);
+
+        ProcessResource processResource = proxy(ProcessResource.class);
+        StartProcessResponse spr = processResource.start(new ByteArrayInputStream(payload));
+        assertNotNull(spr.getInstanceId());
+
+        // ---
+
+        ProcessStatusResponse pir = waitForCompletion(processResource, spr.getInstanceId());
+
+        // ---
+
+        byte[] ab = getLog(pir.getLogFileName());
+        assertLog(".*Hello, " + username + ".*", ab);
+    }
+}
