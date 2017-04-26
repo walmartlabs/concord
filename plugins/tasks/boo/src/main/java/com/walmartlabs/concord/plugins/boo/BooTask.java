a43f40c8b1ab51420b9147be6f0b1b4a71e2660f
jayeshpise <jayesh.pise@gmail.com>
2017-04-26 01:58:09 -0700

    Use the new method of boo and set the execution context variables for deployment state etc

--- plugins/tasks/boo/src/main/java/com/walmartlabs/concord/plugins/boo/BooTask.java
+++ plugins/tasks/boo/src/main/java/com/walmartlabs/concord/plugins/boo/BooTask.java
@@ -1,5 +1,6 @@
 package com.walmartlabs.concord.plugins.boo;
 
+import com.oneops.api.resource.model.Deployment;
 import com.oneops.boo.BooCli;
 import com.walmartlabs.concord.common.Task;
 import io.takari.bpm.api.ExecutionContext;
@@ -19,9 +20,8 @@ public class BooTask implements Task {
     private static final Logger log = LoggerFactory.getLogger(BooTask.class);
 
     @SuppressWarnings("unchecked")
-    public void run(Map<String, Object> args, String payloadPath) throws Exception {
-        log.info("Inside boo task impl. Input: " + args.get("input"));
-
+    public Deployment run(Map<String, Object> args, String payloadPath) throws Exception {
+        log.info("Inside boo task impl. Boo yaml : " + args.get("booTemplateLocation"));
         Path booTemplatePath = Paths.get(payloadPath).resolve(String.valueOf(args.get("booTemplateLocation")));
         if (!Files.exists(booTemplatePath)) {
             // we can't continue
@@ -34,7 +34,18 @@ public class BooTask implements Task {
 
         BooCli boo = new BooCli();
         boo.init(booTemplatePath.toFile(), null, booTemplateVariables , null);
-        boo.createOrUpdatePlatforms();
+        Deployment deployment = boo.createOrUpdatePlatforms();
+        return deployment;
+    }
+
+    public void run(ExecutionContext ctx, Map<String, Object> args, String payloadPath) throws Exception {
+        Deployment deployment = run(args, payloadPath);
+        if (deployment != null) {
+            ctx.setVariable("nsPath", deployment.getNsPath());
+            ctx.setVariable("deploymentId", deployment.getDeploymentId());
+            ctx.setVariable("deploymentState", deployment.getDeploymentState());
+            ctx.setVariable("host", args.get("host"));
+        }
     }
 
     private static Map<String, String> filterTemplateVariables(Map<String, Object> m) {
