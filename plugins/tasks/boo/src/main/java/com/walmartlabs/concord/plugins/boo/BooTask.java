a17a015098dd7edf501bffe7ec759b3fc2278c5a
jayeshpise <jayesh.pise@gmail.com>
2017-04-29 23:33:21 -0700

    BooTask change: Pull environment's deployed object from oneops and put them in execution ctx

--- plugins/tasks/boo/src/main/java/com/walmartlabs/concord/plugins/boo/BooTask.java
+++ plugins/tasks/boo/src/main/java/com/walmartlabs/concord/plugins/boo/BooTask.java
@@ -1,19 +1,25 @@
 package com.walmartlabs.concord.plugins.boo;
 
+import com.github.kevinsawicki.http.HttpRequest;
+import com.google.gson.*;
 import com.oneops.api.resource.model.Deployment;
 import com.oneops.boo.BooCli;
+import com.oneops.cms.simple.domain.CmsCISimple;
 import com.walmartlabs.concord.common.Task;
+import com.walmartlabs.concord.plugins.boo.oneops.Compute;
+import com.walmartlabs.concord.plugins.boo.oneops.Fqdn;
+import com.walmartlabs.concord.plugins.boo.oneops.Platform;
 import io.takari.bpm.api.ExecutionContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Named;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @Named("boo")
 public class BooTask implements Task {
@@ -39,13 +45,105 @@ public class BooTask implements Task {
     }
 
     public void run(ExecutionContext ctx, Map<String, Object> args, String payloadPath) throws Exception {
-        Deployment deployment = run(args, payloadPath);
-        if (deployment != null) {
-            ctx.setVariable("nsPath", deployment.getNsPath());
-            ctx.setVariable("deploymentId", deployment.getDeploymentId());
-            ctx.setVariable("deploymentState", deployment.getDeploymentState());
+        Deployment ooDeployment = run(args, payloadPath);
+        com.walmartlabs.concord.plugins.boo.oneops.Deployment deployment = new com.walmartlabs.concord.plugins.boo.oneops.Deployment();
+        deployment.setId(ooDeployment.getDeploymentId());
+        deployment.setNsPath(ooDeployment.getNsPath());
+        deployment.setStatus(ooDeployment.getDeploymentState());
+
+        if (deployment != null && deployment.getNsPath() != null && ! deployment.getNsPath().isEmpty()) {
+            ctx.setVariable("deployment", deployment);
             ctx.setVariable("host", args.get("host"));
+
+            //query oneops to get all the CIs in this environment "namespace"
+            String response = HttpRequest.get(args.get("oneopsApiHost") + "/adapter/rest/cm/simple/cis?nsPath="
+                    + deployment.getNsPath() + "&recursive=true")
+                    .accept("application/json").body();
+            log.debug("OO response : " + response);
+
+            Gson gson = new GsonBuilder().registerTypeAdapter(Date.class, new JsonDeserializer() {
+                public Date deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+                    return new Date(json.getAsJsonPrimitive().getAsLong());
+                }
+            }).create();
+
+            CmsCISimple[] cis = gson.fromJson(response, CmsCISimple[].class);
+
+            if (cis != null && cis.length > 0) {
+                List<Platform> platforms = buildPlatforms(cis);
+                ctx.setVariable("platforms", platforms);
+                log.info("set these platforms to ctx: " + platforms);
+            }
+        } else {
+            log.warn("No deployment returned by Boo !");
         }
+    }
+
+    private List<Platform> buildPlatforms(CmsCISimple[] cis) {
+        List<Platform> platforms = new ArrayList<Platform>();
+        for (CmsCISimple ci : cis) {
+            if (ci.getCiClassName().matches("bom.*\\.Fqdn")) {
+                buildFqdn(ci, platforms);
+            } else if (ci.getCiClassName().matches("bom.*\\.Compute")) {
+                buildCompute(ci, platforms);
+            }
+        }
+        return platforms;
+    }
+
+    private void buildFqdn(CmsCISimple ci, List<Platform> platforms) {
+        String nsPath = ci.getNsPath();
+        if (nsPath == null || ! nsPath.contains("/")) {
+            log.warn("nspath invalid for ci id: " + ci.getCiId());
+            return;
+        }
+        String[] nsTokens = nsPath.split("/");
+        String platformName = nsTokens[nsTokens.length - 2];
+        Platform platform = null;
+        for (Platform p : platforms) {
+            if (p.getName().equals(platformName)) {
+                platform = p;
+            }
+        }
+        if (platform == null) {
+            platform = new Platform();
+            platform.setName(platformName);
+            platforms.add(platform);
+        }
+
+        Fqdn fqdn = new Fqdn();
+        fqdn.setEntries(ci.getCiAttributes().get("entries").replace("\"", ""));
+        fqdn.setAliases(ci.getCiAttributes().get("aliases").replace("\"", ""));
+        fqdn.setFullAliases(ci.getCiAttributes().get("full_aliases").replace("\"", ""));
+
+        platform.addFqdn(fqdn);
+    }
+
+    private void buildCompute(CmsCISimple ci, List<Platform> platforms) {
+        String nsPath = ci.getNsPath();
+        if (nsPath == null || ! nsPath.contains("/")) {
+            log.warn("nspath invalid for ci id: " + ci.getCiId());
+            return;
+        }
+        String[] nsTokens = nsPath.split("/");
+        String platformName = nsTokens[nsTokens.length - 2];
+        Platform platform = null;
+        for (Platform p : platforms) {
+            if (p.getName().equals(platformName)) {
+                platform = p;
+            }
+        }
+        if (platform == null) {
+            platform = new Platform();
+            platform.setName(platformName);
+            platforms.add(platform);
+        }
+
+        Compute compute = new Compute();
+        compute.setPrivateIp(ci.getCiAttributes().get("private_ip"));
+        compute.setPublicIp(ci.getCiAttributes().get("public_ip"));
+
+        platform.addCompute(compute);
     }
 
     private static Map<String, String> filterTemplateVariables(Map<String, Object> m) {
