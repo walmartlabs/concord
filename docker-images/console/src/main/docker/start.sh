d477d8da003bbac49b77c18403e002a56e6ff8df
Ivan Bodrov <ibodrov@gmail.com>
2017-06-01 20:46:35 -0400

    docker-images: optional ssl for the console

--- docker-images/console/src/main/docker/start.sh
+++ docker-images/console/src/main/docker/start.sh
@@ -3,7 +3,16 @@
 SERVER_ADDR="$SERVER_PORT_8001_TCP_ADDR"
 SERVER_PORT="$SERVER_PORT_8001_TCP_PORT"
 
+NGINX_CONF_TEMPLATE="app.template"
+if [ "${SSL}" == "true" ]; then
+    NGINX_CONF_TEMPLATE="app.ssl.template"
+fi
+
 NGINX_CONF_DIR="/opt/concord/console/nginx/conf"
 
-/usr/bin/sed "s/SERVER_ADDR/$SERVER_ADDR/g;s/SERVER_PORT/$SERVER_PORT/g" ${NGINX_CONF_DIR}/app.template > ${NGINX_CONF_DIR}/app.conf
+cp ${NGINX_CONF_DIR}/${NGINX_CONF_TEMPLATE} ${NGINX_CONF_DIR}/app.conf
+
+/usr/bin/sed -i "s,SERVER_ADDR,$SERVER_ADDR,g;s,SERVER_PORT,$SERVER_PORT,g" ${NGINX_CONF_DIR}/app.conf
+/usr/bin/sed -i "s,SSL_CERT_PATH,$SSL_CERT_PATH,g" ${NGINX_CONF_DIR}/app.conf
+
 /usr/sbin/nginx -c ${NGINX_CONF_DIR}/nginx.conf
