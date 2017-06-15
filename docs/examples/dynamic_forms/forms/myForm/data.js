48c0ca71d6a43bcf930a3bc708c537e19be3a401
Ivan Bodrov <ibodrov@gmail.com>
2017-06-15 18:36:05 -0400

    docs: example of a dynamic form

---
+++ docs/examples/dynamic_forms/forms/myForm/data.js
@@ -0,0 +1,13 @@
+data = {
+    "submitUrl" : "/api/service/custom_form/36cea620-8b75-45d7-a53a-cb1c094e1b34/bdcd87b0-07ad-41dd-8c8d-b25942621e27/continue",
+    "definitions" : {
+        "colors" : {
+            "label" : "colors",
+            "type" : "string",
+            "cardinality" : "AT_LEAST_ONE"
+        }
+    },
+    "values" : {
+        "colors" : [ "red", "green", "blue" ]
+    }
+};
