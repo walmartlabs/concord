b04629961678c705fdaf8752b42c145e729b1b71
Ivan Bodrov <ibodrov@gmail.com>
2017-06-30 10:50:12 -0400

    concord-console: assume aligned data when rending log files

--- console/src/process/log/api.js
+++ console/src/process/log/api.js
@@ -16,30 +16,18 @@ const parseRange = (s: string): FetchRange => {
 
     return {
         unit: "bytes",
-        length: parseInt(m[3]),
-        low: parseInt(m[1]),
-        high: parseInt(m[2])
+        length: parseInt(m[3], 10),
+        low: parseInt(m[1], 10),
+        high: parseInt(m[2], 10)
     };
 };
 
 const offsetRange = (data: string, range: FetchRange) => {
-    // if our data starts from the beginning, do nothing
-    if (range.low !== undefined && range.low <= 0) {
-        return {data, range};
-    }
-
-    // seek to the nearest newline
-    const i = data.indexOf("\n");
-
-    // no line breaks or a single line, do nothing
-    if (i < 0 || i + 1 >= data.length) {
-        return {data, range};
-    }
-
-    // trim data to a new line, add trimmed offest to the start of a range
+    // noop, we assume that the data is aligned by \n
+    // this will work only with our current implementation of the API
     return {
-        data: data.substr(i + 1),
-        range: {...range, low: range.low + i + 1}
+        data,
+        range
     };
 };
 
