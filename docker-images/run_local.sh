0ee1f99f9eba7dcefd1af83e52687e860b6597a5
Ivan Bodrov <ibodrov@gmail.com>
2017-07-12 11:03:31 -0400

    docker-images: install ansible into the agent to preserve backwards compatibility, update run scripts

--- docker-images/run_local.sh
+++ docker-images/run_local.sh
@@ -5,30 +5,32 @@ docker rm -f db dind agent server console
 docker run -d \
 --name db \
 -e 'POSTGRES_PASSWORD=q1' \
+--network=host \
 hub.docker.prod.walmart.com/library/postgres:latest
 
 docker run -d \
--p 8001:8001 \
 --name server \
---link db \
+-v /tmp:/tmp \
 -v /opt/concord/conf/ldap.properties:/opt/concord/conf/ldap.properties:ro \
 -e 'LDAP_CFG=/opt/concord/conf/ldap.properties' \
--e 'DB_URL=jdbc:postgresql://db:5432/postgres' \
+-e 'DB_URL=jdbc:postgresql://localhost:5432/postgres' \
+--network=host \
 walmartlabs/concord-server
 
 docker run -d \
 --privileged \
 --name dind \
+--network=host \
 docker.prod.walmart.com/walmartlabs/concord-dind
 
 docker run -d \
 --name agent \
---link dind \
---link server \
+-v /tmp:/tmp \
+-e 'DOCKER_HOST=tcp://localhost:2375' \
+--network=host \
 walmartlabs/concord-agent
 
 docker run -d \
--p 8080:8080 \
 --name console \
---link server \
+--network=host \
 walmartlabs/concord-console
