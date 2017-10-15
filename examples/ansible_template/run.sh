70c54df2ef4a19eca3793d44bae910622397bc3c
Ivan Bodrov <ibodrov@gmail.com>
2017-10-15 19:30:43 -0400

    examples: fix content type

--- examples/ansible_template/run.sh
+++ examples/ansible_template/run.sh
@@ -8,4 +8,4 @@ cp -R playbook target/
 cd target && zip -r payload.zip ./* > /dev/null && cd ..
 
 read -p "Username: " CURL_USER
-curl -u ${CURL_USER} -H "Content-Type: application/octet-stream" -F archive=@target/payload.zip -F request=@request.json http://${SERVER_ADDR}/api/v1/process
+curl -u ${CURL_USER} -F archive=@target/payload.zip -F request=@request.json http://${SERVER_ADDR}/api/v1/process
