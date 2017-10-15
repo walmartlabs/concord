57ecc6061a99c123740ca097235c223b4203775c
Ivan Bodrov <ibodrov@gmail.com>
2017-10-15 14:07:01 -0400

    examples: tidy up, use LDAP auth, update dependencies

--- examples/ansible_template/run.sh
+++ examples/ansible_template/run.sh
@@ -7,5 +7,5 @@ cp -R playbook target/
 
 cd target && zip -r payload.zip ./* > /dev/null && cd ..
 
-echo "Sending the payload..."
-curl -H "Authorization: auBy4eDWrKWsyhiDp3AQiw" -F archive=@target/payload.zip -F request=@request.json http://${SERVER_ADDR}/api/v1/process
+read -p "Username: " CURL_USER
+curl -u ${CURL_USER} -H "Content-Type: application/octet-stream" -F archive=@target/payload.zip -F request=@request.json http://${SERVER_ADDR}/api/v1/process
--- examples/form_and_long_process/run.sh
+++ examples/form_and_long_process/run.sh
@@ -7,4 +7,5 @@ cp -R concord.yml target/
 
 cd target && zip -r payload.zip ./* > /dev/null && cd ..
 
-curl -H "Authorization: auBy4eDWrKWsyhiDp3AQiw" -H "Content-Type: application/octet-stream" --data-binary @target/payload.zip http://${SERVER_ADDR}/api/v1/process
+read -p "Username: " CURL_USER
+curl -u ${CURL_USER} -H "Content-Type: application/octet-stream" --data-binary @target/payload.zip http://${SERVER_ADDR}/api/v1/process
