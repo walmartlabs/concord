06e7cc11b8d620d605feec2d55cb88740018d5b1
Ivan Bodrov <ibodrov@gmail.com>
2017-10-20 11:19:40 -0400

    add the inventory's db user params

--- docker-images/server/src/main/docker/start.sh
+++ docker-images/server/src/main/docker/start.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 APP_DIR="/opt/concord/server/"
 
-if [ "$DB" == "postgresql" ]; then
-    export DB_DIALECT="POSTGRES_9_5"
-    export DB_DRIVER="org.postgresql.Driver"
-    export DB_URL="jdbc:postgresql://$PG_PORT_5432_TCP_ADDR:$PG_PORT_5432_TCP_PORT/postgres"
-    export DB_USERNAME="postgres"
-fi;
-
 export DEPS_STORE_DIR="$APP_DIR";
 
 exec java \
