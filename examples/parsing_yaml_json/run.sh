5d1b64e9c778f3b9353f048f23d16de2a1b958c5
Ivan Bodrov <ibodrov@gmail.com>
2017-10-24 17:28:47 -0400

    examples: how to parse an external yaml or json file

---
+++ examples/parsing_yaml_json/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SERVER_ADDR="$1"
+
+rm -rf target && mkdir target
+cp -R concord.yml test.json test.yml target/
+
+cd target && zip -r payload.zip ./* > /dev/null && cd ..
+
+read -p "Username: " CURL_USER
+curl -u ${CURL_USER} -H "Content-Type: application/octet-stream" --data-binary @target/payload.zip http://${SERVER_ADDR}/api/v1/process
