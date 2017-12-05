145577386d35c46fdf379eab53b0121d3d800be7
Ivan Bodrov <ibodrov@gmail.com>
2017-12-05 17:52:52 -0500

    docker-images: small improvement for the push script

--- docker-images/push.sh
+++ docker-images/push.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-TAG="$1"
+SRC_TAG="$1"
+DST_TAG="$2"
 
-docker tag walmartlabs/concord-base:${TAG} docker.prod.walmart.com/walmartlabs/concord-base:${TAG}
-docker push docker.prod.walmart.com/walmartlabs/concord-base:${TAG}
+docker tag walmartlabs/concord-base:${SRC_TAG} docker.prod.walmart.com/walmartlabs/concord-base:${DST_TAG}
+docker push docker.prod.walmart.com/walmartlabs/concord-base:${DST_TAG}
 
-docker tag walmartlabs/concord-server:${TAG} docker.prod.walmart.com/walmartlabs/concord-server:${TAG}
-docker push docker.prod.walmart.com/walmartlabs/concord-server:${TAG}
+docker tag walmartlabs/concord-server:${SRC_TAG} docker.prod.walmart.com/walmartlabs/concord-server:${DST_TAG}
+docker push docker.prod.walmart.com/walmartlabs/concord-server:${DST_TAG}
 
-docker tag walmartlabs/concord-console:${TAG} docker.prod.walmart.com/walmartlabs/concord-console:${TAG}
-docker push docker.prod.walmart.com/walmartlabs/concord-console:${TAG}
+docker tag walmartlabs/concord-console:${SRC_TAG} docker.prod.walmart.com/walmartlabs/concord-console:${DST_TAG}
+docker push docker.prod.walmart.com/walmartlabs/concord-console:${DST_TAG}
 
-docker tag walmartlabs/concord-agent:${TAG} docker.prod.walmart.com/walmartlabs/concord-agent:${TAG}
-docker push docker.prod.walmart.com/walmartlabs/concord-agent:${TAG}
+docker tag walmartlabs/concord-agent:${SRC_TAG} docker.prod.walmart.com/walmartlabs/concord-agent:${DST_TAG}
+docker push docker.prod.walmart.com/walmartlabs/concord-agent:${DST_TAG}
 
-docker tag walmartlabs/concord-ansible:${TAG} docker.prod.walmart.com/walmartlabs/concord-ansible:${TAG}
-docker push docker.prod.walmart.com/walmartlabs/concord-ansible:${TAG}
+docker tag walmartlabs/concord-ansible:${SRC_TAG} docker.prod.walmart.com/walmartlabs/concord-ansible:${DST_TAG}
+docker push docker.prod.walmart.com/walmartlabs/concord-ansible:${DST_TAG}
