5148f537be9c30e8e3b760164861bcb793424055
Ivan Bodrov <ibodrov@gmail.com>
2017-12-13 12:00:10 -0500

    concord-console: projects are PUBLIC by default

--- console/src/project/crud.js
+++ console/src/project/crud.js
@@ -2,7 +2,7 @@
 import DataItem from "../shared/DataItem";
 import * as api from "./api";
 
-const {actions, reducers, selectors, sagas} = DataItem("project", {},
+const {actions, reducers, selectors, sagas} = DataItem("project", {visibility: "PUBLIC"},
     api.fetchProject,
     api.updateProject,
     api.deleteProject);
