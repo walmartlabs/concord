34820962967d21987e9ac727545b49b0748aabe1
Yury Brigadirenko - vn0tj0b <YBrigadirenko@walmartlabs.com>
2018-06-28 18:25:32 +0300

    sdk: context variables utils (#487)

---
+++ sdk/src/main/java/com/walmartlabs/concord/sdk/ContextUtils.java
@@ -0,0 +1,108 @@
+package com.walmartlabs.concord.sdk;
+
+/*-
+ * *****
+ * Concord
+ * -----
+ * Copyright (C) 2017 - 2018 Walmart Inc.
+ * -----
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =====
+ */
+
+import java.util.List;
+import java.util.Map;
+
+public final class ContextUtils {
+
+    public static int getInt(Context ctx, String name, int defaultValue) {
+        return getVariable(ctx, name, defaultValue, Integer.class);
+    }
+
+    public static Number getNumber(Context ctx, String name, Number defaultValue) {
+        return getVariable(ctx, name, defaultValue, Number.class);
+    }
+
+    public static String getString(Context ctx, String name, String defaultValue) {
+        return getVariable(ctx, name, defaultValue, String.class);
+    }
+
+    public static boolean getBoolean(Context ctx, String name, boolean defaultValue) {
+        return getVariable(ctx, name, defaultValue, Boolean.class);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <K, V> Map<K, V> getMap(Context ctx, String name, Map<K, V> defaultValue) {
+        return getVariable(ctx, name, defaultValue, Map.class);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <E> List<E> getList(Context ctx, String name, List<E> defaultValue) {
+        return getVariable(ctx, name, defaultValue, List.class);
+    }
+
+    public static int assertInt(Context ctx, String name) {
+        return assertVariable(ctx, name, Integer.class);
+    }
+
+    public static Number assertNumber(Context ctx, String name) {
+        return assertVariable(ctx, name, Number.class);
+    }
+
+    public static String assertString(Context ctx, String name) {
+        return assertVariable(ctx, name, String.class);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <K, V> Map<K, V> assertMap(Context ctx, String name) {
+        return assertVariable(ctx, name, Map.class);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <E> List<E> assertList(Context ctx, String name) {
+        return assertVariable(ctx, name, List.class);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> T getVariable(Context ctx, String name, T defaultValue) {
+        T result = (T)ctx.getVariable(name);
+        if (result != null) {
+            return result;
+        }
+        return defaultValue;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> T getVariable(Context ctx, String name, T defaultValue, Class<T> type) {
+        Object value = getVariable(ctx, name, defaultValue);
+        if (value == null) {
+            return null;
+        }
+
+        if (type.isInstance(value)) {
+            return type.cast(value);
+        }
+        throw new IllegalArgumentException("Invalid variable '" + name + "' type, expected: " + type + ", got: " + value.getClass());
+    }
+
+    public static <T> T assertVariable(Context ctx, String name, Class<T> type) {
+        T result = getVariable(ctx, name, null, type);
+        if (result != null) {
+            return result;
+        }
+        throw new IllegalArgumentException("Mandatory variable '" + name + "' is required");
+    }
+
+    private ContextUtils() {
+    }
+}
---
+++ sdk/src/test/java/com/walmartlabs/concord/sdk/ContextUtilsTest.java
@@ -0,0 +1,175 @@
+package com.walmartlabs.concord.sdk;
+
+/*-
+ * *****
+ * Concord
+ * -----
+ * Copyright (C) 2017 - 2018 Walmart Inc.
+ * -----
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =====
+ */
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+public class ContextUtilsTest {
+
+    @Test
+    public void testGetInt() {
+        Context ctx = new Ctx();
+        ctx.setVariable("k", 123);
+        ctx.setVariable("not-int", 123L);
+
+        // ---
+        Integer result = ContextUtils.getVariable(ctx, "k", null, Integer.class);
+        assertNotNull(result);
+        assertEquals((Integer)123, result);
+
+        // ---
+        result = ContextUtils.getVariable(ctx, "not-found", null, Integer.class);
+        assertNull(result);
+
+        // ---
+        result = ContextUtils.getVariable(ctx, "not-found", -1, Integer.class);
+        assertNotNull(result);
+        assertEquals((Integer)(-1), result);
+
+        // ---
+        int res = ContextUtils.getInt(ctx, "k", -1);
+        assertEquals(123, res);
+
+        // ---
+        res = ContextUtils.getInt(ctx, "not-found", -1);
+        assertEquals(-1, res);
+
+        // ---
+        try {
+            ContextUtils.getVariable(ctx, "not-int", null, Integer.class);
+            fail("exception expected");
+        } catch (IllegalArgumentException e) {
+            // do nothing
+        }
+    }
+
+    @Test
+    public void testGetNumber() {
+        Context ctx = new Ctx();
+        ctx.setVariable("k", 123L);
+        ctx.setVariable("not-number", "boom");
+
+        // ---
+        Number result = ContextUtils.getVariable(ctx, "k", null, Number.class);
+        assertNotNull(result);
+        assertEquals(123L, result);
+
+        // ---
+        result = ContextUtils.getVariable(ctx, "not-found", null, Number.class);
+        assertNull(result);
+
+        // ---
+        result = ContextUtils.getVariable(ctx, "not-found", -1, Number.class);
+        assertNotNull(result);
+        assertEquals(-1, result);
+
+
+        // ---
+        result = ContextUtils.getNumber(ctx, "k", -1);
+        assertEquals(123L, result);
+
+        // ---
+        result = ContextUtils.getNumber(ctx, "not-found", -1);
+        assertEquals(-1, result);
+
+        // ---
+        try {
+            ContextUtils.getVariable(ctx, "not-number", null, Number.class);
+            fail("exception expected");
+        } catch (IllegalArgumentException e) {
+            // do nothing
+        }
+
+        // ---
+        result = ContextUtils.assertNumber(ctx, "k");
+        assertNotNull(result);
+        assertEquals(123L, result);
+
+        // ---
+        try {
+            ContextUtils.assertNumber(ctx, "not-found");
+            fail("exception expected");
+        } catch (IllegalArgumentException e) {
+            // do nothing
+        }
+    }
+
+    private static class Ctx implements Context {
+
+        private final Map<String, Object> vars = new HashMap<>();
+
+        @Override
+        public Object getVariable(String key) {
+            return vars.get(key);
+        }
+
+        @Override
+        public void setVariable(String key, Object value) {
+            vars.put(key, value);
+        }
+
+        @Override
+        public void removeVariable(String key) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public Set<String> getVariableNames() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public <T> T eval(String expr, Class<T> type) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public Object interpolate(Object v) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public Map<String, Object> toMap() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void suspend(String eventName) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public String getProcessDefinitionId() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public String getElementId() {
+            throw new UnsupportedOperationException();
+        }
+    }
+}
