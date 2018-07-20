eb8339be1b134cf286472741bf4f3d0fc66c0c71
Ivan Bodrov <ibodrov@gmail.com>
2018-07-20 19:54:51 -0400

    examples: tidy up

--- examples/groovy/test.groovy
+++
@@ -1,5 +0,0 @@
-@GrabResolver(name = 'walmart', root = 'https://repository.walmart.com/nexus/content/groups/public')
-@Grab(group='org.apache.commons', module='commons-lang3', version='3.7')
-import org.apache.commons.lang3.RandomStringUtils
-
-println RandomStringUtils.random(10)
