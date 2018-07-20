16c26459b02d3b1eae2b6de6e124c5ba1a2a7865
Prasanth Pendham - ppendha <Prasanth.Pendham@walmart.com>
2018-07-20 16:08:27 -0500

    STRDTORC-142: better validation of form step in .concord.yml (#540)

--- project-model/src/main/java/com/walmartlabs/concord/project/yaml/converter/YamlFormCallConverter.java
+++ project-model/src/main/java/com/walmartlabs/concord/project/yaml/converter/YamlFormCallConverter.java
@@ -9,9 +9,9 @@ package com.walmartlabs.concord.project.yaml.converter;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,6 +20,7 @@ package com.walmartlabs.concord.project.yaml.converter;
  * =====
  */
 
+import com.fasterxml.jackson.core.JsonLocation;
 import com.walmartlabs.concord.project.InternalConstants;
 import com.walmartlabs.concord.project.yaml.YamlConverterException;
 import com.walmartlabs.concord.project.yaml.model.YamlFormCall;
@@ -29,17 +30,23 @@ import io.takari.bpm.model.form.FormExtension;
 import java.util.*;
 import java.util.stream.Collectors;
 
+
 public class YamlFormCallConverter implements StepConverter<YamlFormCall> {
 
+    private static final List<String> SUPPORTED_FORM_OPTIONS = Arrays.asList("yield", "runAs", "values");
+
     @Override
+    @SuppressWarnings("unchecked")
     public Chunk convert(ConverterContext ctx, YamlFormCall s) throws YamlConverterException {
         Chunk c = new Chunk();
-
         String id = ctx.nextId();
+
         Map<String, Object> opts = (Map<String, Object>) StepConverter.deepConvert(s.getOptions());
         if (opts != null && opts.isEmpty()) {
             opts = null;
         }
+
+        validate(opts, s.getLocation());
 
         if (opts != null && opts.get(InternalConstants.Forms.RUN_AS_KEY) != null) {
             Map<String, Object> runAsParams = (Map<String, Object>) opts.get(InternalConstants.Forms.RUN_AS_KEY);
@@ -55,6 +62,7 @@ public class YamlFormCallConverter implements StepConverter<YamlFormCall> {
         return c;
     }
 
+    @SuppressWarnings("unchecked")
     private Map<String, Object> getGroupsMap(Object ldapObj) {
         List<String> groupList = null;
         if (ldapObj instanceof List) {
@@ -67,5 +75,21 @@ public class YamlFormCallConverter implements StepConverter<YamlFormCall> {
         }
 
         return Collections.singletonMap(InternalConstants.Forms.RUN_AS_GROUP_KEY, groupList);
+    }
+
+    private static void validate(Map<String, Object> opts, JsonLocation loc) {
+        if (opts == null) {
+            return;
+        }
+
+        Set<String> keys = new HashSet<>(opts.keySet());
+        keys.removeAll(SUPPORTED_FORM_OPTIONS);
+
+        if (keys.isEmpty()) {
+            return;
+        }
+
+        throw new IllegalArgumentException("'" + keys + "' are not form supported options. Supported options are only:"
+                + SUPPORTED_FORM_OPTIONS + ". Error in form step @:" + opts);
     }
 }
