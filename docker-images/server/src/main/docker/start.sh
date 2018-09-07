9ad2d6eb32f3b4231bc0d18785916266ed0b5ca8
Ivan Bodrov - vn03g8w <IBodrov@walmartlabs.com>
2018-09-07 10:20:12 -0400

    docker: allow custom JVM params in the server container (#631)

--- docker-images/server/src/main/docker/start.sh
+++ docker-images/server/src/main/docker/start.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
-MAIN_CLASS="com.walmartlabs.concord.server.Main"
+APP_DIR="/opt/concord/server"
 
+MAIN_CLASS="com.walmartlabs.concord.server.Main"
 if [ "${CONCORD_COMMAND}" = "migrateDb" ]; then
     MAIN_CLASS="com.walmartlabs.concord.server.MigrateDB"
 fi
 
-APP_DIR="/opt/concord/server"
-
 CFG_FILE="";
-if [ ! -z "$CONCORD_CFG_FILE" ]; then
-    CFG_FILE="-Dollie.conf=$CONCORD_CFG_FILE"
+if [ ! -z "${CONCORD_CFG_FILE}" ]; then
+    CFG_FILE="-Dollie.conf=${CONCORD_CFG_FILE}"
 fi
 
 GC_LOG_DIR=${GC_LOG_DIR:-"${APP_DIR}/logs/gc"}
 mkdir -p ${GC_LOG_DIR}
-
 echo "GC logs: ${GC_LOG_DIR}"
 
+if [ -z "${CONCORD_JAVA_OPTS}" ]; then
+    CONCORD_JAVA_OPTS="-Xms2g -Xmx2g -server"
+fi
+echo "CONCORD_JAVA_OPTS: ${CONCORD_JAVA_OPTS}"
+
 exec java \
--Xms2g \
--Xmx2g \
+${CONCORD_JAVA_OPTS} \
 -XX:+PrintGC \
 -XX:+PrintGCTimeStamps \
 -XX:+PrintGCDateStamps \
@@ -34,6 +36,6 @@ exec java \
 -server \
 -Djava.net.preferIPv4Stack=true \
 -Djava.security.egd=file:/dev/./urandom \
-$CFG_FILE \
+${CFG_FILE} \
 -cp "${APP_DIR}/*" \
 "${MAIN_CLASS}"
