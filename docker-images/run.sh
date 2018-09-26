f55e8be09e9d13f0f2ee428c508190a671264b5c
Ivan Bodrov <ibodrov@gmail.com>
2018-09-26 11:55:04 -0400

    docker: update the startup scripts

--- docker-images/run.sh
+++ docker-images/run.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
-VERSION="0.76.0"
+VERSION="0.88.2"
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ -z "$CONCORD_CFG_FILE" ]; then
+    CONCORD_CFG_FILE="${BASE_DIR}/server.conf"
+fi
+echo "CONCORD_CFG_FILE: ${CONCORD_CFG_FILE}"
 
 docker rm -f db dind agent server console
 
 docker run -d \
 --name db \
--e 'POSTGRES_PASSWORD=q1' \
-hub.docker.prod.walmart.com/library/postgres:latest
+-e "POSTGRES_PASSWORD=q1" \
+hub.docker.prod.walmart.com/library/postgres:10.4-alpine
 
 docker pull docker.prod.walmart.com/walmartlabs/concord-server:${VERSION}
 docker run -d \
 -p 8001:8001 \
 --name server \
 --link db \
--v /opt/concord/conf/ldap.properties:/opt/concord/conf/ldap.properties:ro \
--v /tmp:/tmp \
--e 'LDAP_CFG=/opt/concord/conf/ldap.properties' \
--e 'DB_URL=jdbc:postgresql://db:5432/postgres' \
--e 'GC_LOG_DIR=/tmp/server/gc' \
+-v "${CONCORD_CFG_FILE}:/opt/concord/conf/server.conf:ro" \
+-e "DB_URL=jdbc:postgresql://db:5432/postgres" \
+-e "CONCORD_CFG_FILE=/opt/concord/conf/server.conf" \
 docker.prod.walmart.com/walmartlabs/concord-server:${VERSION}
 
 docker pull docker.prod.walmart.com/walmartlabs/concord-dind
@@ -35,9 +38,10 @@ docker run -d \
 --link dind \
 --link server \
 -v /tmp:/tmp \
--v ${BASE_DIR}/mvn.json:/opt/concord/conf/mvn.json:ro \
--e 'CONCORD_MAVEN_CFG=/opt/concord/conf/mvn.json' \
--e 'CONCORD_DOCKER_LOCAL_MODE=false' \
+-v "${BASE_DIR}/mvn.json:/opt/concord/conf/mvn.json:ro" \
+-e "CONCORD_MAVEN_CFG=${BASE_URL}/mvn.json" \
+-e "CONCORD_DOCKER_LOCAL_MODE=false" \
+-e "SERVER_API_BASE_URL=http://server:8001" \
 docker.prod.walmart.com/walmartlabs/concord-agent:${VERSION}
 
 docker pull docker.prod.walmart.com/walmartlabs/concord-console:${VERSION}
