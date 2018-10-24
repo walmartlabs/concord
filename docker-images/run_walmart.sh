a9c69c6751c6748990d1ddf289dcc4b7bb95fb4a
Ivan Bodrov <ibodrov@gmail.com>
2018-10-24 15:45:21 -0400

    docker: replace old startup scripts with a new walmart-specific one

--- docker-images/run_walmart.sh
+++ docker-images/run_walmart.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-VERSION="0.68.2"
+if [ -z $VERSION ]; then
+    VERSION="0.92.0"
+fi
+echo "VERSION: ${VERSION}"
 
 if [ -z $CONCORD_CFG_FILE ]; then
     CONCORD_CFG_FILE="/opt/concord/conf/server.conf"
 fi
 echo "CONCORD_CFG_FILE: ${CONCORD_CFG_FILE}"
 
-docker rm -f db dind agent server console
+echo "Removing old containers..."
+docker rm -f db dind agent server console > /dev/null
 
 docker run -d \
 --name db \
@@ -15,7 +19,7 @@ docker run -d \
 -e 'PGDATA=/var/lib/postgresql/data/pgdata' \
 --mount source=concordDB,target=/var/lib/postgresql/data \
 -p 5432:5432 \
-hub.docker.prod.walmart.com/library/postgres:latest
+hub.docker.prod.walmart.com/library/postgres:10.4-alpine
 
 docker run -d \
 --link db \
@@ -23,9 +27,17 @@ docker run -d \
 -p 8001:8001 \
 -v /tmp:/tmp \
 -v "${CONCORD_CFG_FILE}:${CONCORD_CFG_FILE}:ro" \
--e "CONCORD_CFG_FILE=$CONCORD_CFG_FILE" \
+-e "CONCORD_CFG_FILE=${CONCORD_CFG_FILE}" \
 -e 'DB_URL=jdbc:postgresql://db:5432/postgres' \
 docker.prod.walmart.com/walmartlabs/concord-server:${VERSION}
+
+# wait for the server to start
+echo -n "Waiting for the server to start"
+until $(curl --output /dev/null --silent --head --fail "http://localhost:8001/api/v1/server/ping"); do
+    printf '.'
+    sleep 1
+done
+echo "done!"
 
 docker run -d \
 --privileged \
@@ -39,10 +51,12 @@ docker run -d \
 --link server \
 -v /tmp:/tmp \
 -e 'CONCORD_DOCKER_LOCAL_MODE=false' \
+-e 'SERVER_API_BASE_URL=http://server:8001' \
 docker.prod.walmart.com/walmartlabs/concord-agent:${VERSION}
 
 docker run -d \
 --name console \
 --link server \
 -p 8080:8080 \
+-v /tmp/console/logs:/opt/concord/logs \
 docker.prod.walmart.com/walmartlabs/concord-console:${VERSION}
