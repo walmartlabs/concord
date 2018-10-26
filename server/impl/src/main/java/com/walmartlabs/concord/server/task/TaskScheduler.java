d3a73c9441d949ca80dccbf2542fb8c18d086da3
Yury Brigadirenko - vn0tj0b <YBrigadirenko@walmartlabs.com>
2018-10-26 17:51:43 +0300

    concord-server: handle stalled tasks (#726)

--- server/impl/src/main/java/com/walmartlabs/concord/server/task/TaskScheduler.java
+++ server/impl/src/main/java/com/walmartlabs/concord/server/task/TaskScheduler.java
@@ -9,9 +9,9 @@ package com.walmartlabs.concord.server.task;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,6 +24,7 @@ import com.walmartlabs.concord.db.AbstractDao;
 import com.walmartlabs.concord.db.PgUtils;
 import com.walmartlabs.concord.server.PeriodicTask;
 import org.jooq.Configuration;
+import org.jooq.DSLContext;
 import org.jooq.Field;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,12 +33,12 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
 import java.sql.Timestamp;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+import static com.walmartlabs.concord.db.PgUtils.interval;
 import static com.walmartlabs.concord.server.jooq.Tables.TASKS;
 import static org.jooq.impl.DSL.currentTimestamp;
 import static org.jooq.impl.DSL.value;
@@ -50,10 +51,12 @@ public class TaskScheduler extends PeriodicTask {
 
     private static final long POLL_INTERVAL = TimeUnit.SECONDS.toMillis(1);
     private static final long ERROR_DELAY = TimeUnit.SECONDS.toMillis(10);
+    private static final String MAX_STALLED_AGE = "1 minute";
 
     private final ExecutorService executor;
     private final SchedulerDao dao;
     private final Map<String, ScheduledTask> tasks;
+    private final Set<String> runningTasks = Collections.synchronizedSet(new HashSet<>());
 
     @Inject
     public TaskScheduler(Map<String, ScheduledTask> tasks, SchedulerDao dao) {
@@ -74,6 +77,10 @@ public class TaskScheduler extends PeriodicTask {
         }
 
         ids.forEach(this::startTask);
+
+        updateRunningTasks();
+
+        failStalled();
     }
 
     @Override
@@ -91,6 +98,8 @@ public class TaskScheduler extends PeriodicTask {
 
         executor.submit(() -> {
             try {
+                runningTasks.add(id);
+
                 task.performTask();
 
                 dao.success(id);
@@ -100,6 +109,26 @@ public class TaskScheduler extends PeriodicTask {
                 log.error("startTask ['{}'] -> error", id, e);
 
                 dao.fail(id);
+            } finally {
+                runningTasks.remove(id);
+            }
+        });
+    }
+
+    private void updateRunningTasks() {
+        synchronized (runningTasks) {
+            dao.updateRunning(runningTasks);
+        }
+    }
+
+    private void failStalled() {
+        Field<Timestamp> cutOff = currentTimestamp().minus(interval(MAX_STALLED_AGE));
+
+        dao.transaction(tx -> {
+            List<String> ids = dao.pollStalled(tx, cutOff);
+            for(String id : ids) {
+                dao.fail(tx, id);
+                log.info("failStalled -> marked as failed: {}", id);
             }
         });
     }
@@ -134,6 +163,7 @@ public class TaskScheduler extends PeriodicTask {
                         .set(TASKS.STARTED_AT, currentTimestamp())
                         .set(TASKS.TASK_STATUS, value("RUNNING"))
                         .set(TASKS.FINISHED_AT, (Timestamp)null)
+                        .set(TASKS.LAST_UPDATED_AT, currentTimestamp())
                         .where(TASKS.TASK_ID.in(ids))
                         .execute();
 
@@ -141,33 +171,60 @@ public class TaskScheduler extends PeriodicTask {
             });
         }
 
+        public List<String> pollStalled(DSLContext tx, Field<Timestamp> cutOff) {
+            return tx.select(TASKS.TASK_ID)
+                    .from(TASKS)
+                    .where(TASKS.LAST_UPDATED_AT.lessThan(cutOff)
+                            .and(TASKS.FINISHED_AT.isNull())
+                            .and(TASKS.TASK_INTERVAL.greaterThan(0L)))
+                    .forUpdate()
+                    .skipLocked()
+                    .fetch(TASKS.TASK_ID);
+        }
+
         public void success(String taskId) {
-            taskFinished(taskId, "OK");
+            tx(tx -> taskFinished(tx, taskId, "OK"));
         }
 
         public void fail(String taskId) {
-            taskFinished(taskId, "ERROR");
+            tx(tx -> fail(tx, taskId));
+        }
+
+        public void fail(DSLContext tx, String taskId) {
+            taskFinished(tx, taskId, "ERROR");
         }
 
         public void updateTaskIntervals(Map<String, ScheduledTask> tasks) {
             tx(tx -> {
                 for (Map.Entry<String, ScheduledTask> e : tasks.entrySet()) {
                     tx.insertInto(TASKS, TASKS.TASK_ID, TASKS.TASK_INTERVAL)
-                        .values(e.getKey(), e.getValue().getIntervalInSec())
-                        .onDuplicateKeyUpdate().set(TASKS.TASK_INTERVAL, e.getValue().getIntervalInSec())
-                        .execute();
+                            .values(e.getKey(), e.getValue().getIntervalInSec())
+                            .onDuplicateKeyUpdate().set(TASKS.TASK_INTERVAL, e.getValue().getIntervalInSec())
+                            .execute();
                 }
             });
         }
 
-        private void taskFinished(String taskId, String status) {
+        public void updateRunning(Set<String> runningTasks) {
             tx(tx -> {
                 tx.update(TASKS)
-                        .set(TASKS.FINISHED_AT, currentTimestamp())
-                        .set(TASKS.TASK_STATUS, value(status))
-                        .where(TASKS.TASK_ID.eq(taskId))
+                        .set(TASKS.LAST_UPDATED_AT, currentTimestamp())
+                        .where(TASKS.TASK_ID.in(runningTasks))
                         .execute();
             });
+        }
+
+        private void transaction(Tx t) {
+            tx(t);
+        }
+
+        private void taskFinished(DSLContext tx, String taskId, String status) {
+            tx.update(TASKS)
+                    .set(TASKS.FINISHED_AT, currentTimestamp())
+                    .set(TASKS.LAST_UPDATED_AT, currentTimestamp())
+                    .set(TASKS.TASK_STATUS, value(status))
+                    .where(TASKS.TASK_ID.eq(taskId))
+                    .execute();
         }
     }
 }
