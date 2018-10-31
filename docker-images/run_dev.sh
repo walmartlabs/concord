47f4e8288044c0ce3e2b2c888aa2f33cbc4b75aa
Anders Janmyr - vn0wxf9 <AJanmyr@walmartlabs.com>
2018-10-31 16:25:46 -0700

    docker: add run_dev.sh for local docker development of plugins (#715)
    The local .m2 repo must override the container repo, otherwise the
    container will cache the snapshots and not update when new version
    become available.
    
    Also added check that the CONCORD_CFG_FILE has been updated with proper
    AD credentials.

---
+++ docker-images/run_dev.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ -z $VERSION ]; then
+    VERSION="0.92.0"
+fi
+echo "VERSION: ${VERSION}"
+
+if [ -z $CONCORD_CFG_FILE ]; then
+    CONCORD_CFG_FILE=${BASE_DIR}/server.conf
+fi
+echo "CONCORD_CFG_FILE: ${CONCORD_CFG_FILE}"
+
+if grep 'AD_' "${CONCORD_CFG_FILE}"; then
+    echo "To start, enter your AD credentials into ${CONCORD_CFG_FILE}"
+    echo "If you cannot login after AD creds, change the LDAP config"
+    echo "from ldaps://172.29.104.102:3269 to ldap://172.29.104.102:389"
+    exit 1
+fi
+
+echo "Removing old containers..."
+docker rm -f db dind agent server console > /dev/null
+
+docker run -d \
+--name db \
+-e 'POSTGRES_PASSWORD=q1' \
+-e 'PGDATA=/var/lib/postgresql/data/pgdata' \
+--mount source=concordDB,target=/var/lib/postgresql/data \
+-p 5432:5432 \
+hub.docker.prod.walmart.com/library/postgres:10.4-alpine
+
+docker run -d \
+--link db \
+--name server \
+-p 8001:8001 \
+-v /tmp:/tmp \
+-v "${CONCORD_CFG_FILE}:${CONCORD_CFG_FILE}:ro" \
+-e "CONCORD_CFG_FILE=${CONCORD_CFG_FILE}" \
+-e 'DB_URL=jdbc:postgresql://db:5432/postgres' \
+docker.prod.walmart.com/walmartlabs/concord-server:${VERSION}
+
+# wait for the server to start
+echo -n "Waiting for the server to start"
+until $(curl --output /dev/null --silent --head --fail "http://localhost:8001/api/v1/server/ping"); do
+    printf '.'
+    sleep 1
+done
+echo "done!"
+
+docker run -d \
+--privileged \
+--name dind \
+-v /tmp:/tmp \
+docker.prod.walmart.com/walmartlabs/concord-dind
+
+docker run -d \
+--name agent \
+--link dind \
+--link server \
+-v /tmp:/tmp \
+-v "${HOME}/.m2/repository:/home/concord/.m2/repository" \
+-v "${BASE_DIR}/mvn.json:/opt/concord/conf/mvn.json:ro" \
+-e 'CONCORD_MAVEN_CFG=/opt/concord/conf/mvn.json' \
+-e 'CONCORD_DOCKER_LOCAL_MODE=false' \
+-e 'SERVER_API_BASE_URL=http://server:8001' \
+docker.prod.walmart.com/walmartlabs/concord-agent:${VERSION}
+
+docker run -d \
+--name console \
+--link server \
+-p 8080:8080 \
+-v /tmp/console/logs:/opt/concord/logs \
+docker.prod.walmart.com/walmartlabs/concord-console:${VERSION}
