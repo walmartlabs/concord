7892b5619ed7d248a071bc3958b1cb9ad3cd6986
Yury Brigadirenko - vn0tj0b <YBrigadirenko@walmartlabs.com>
2019-05-02 17:06:16 +0300

    STRDTORC-1049: log-tasks: create separate tasks for warn, error and debug levels (#1035)

---
+++ plugins/tasks/log/src/main/java/com/walmartlabs/concord/plugins/log/LogWarnTask.java
@@ -0,0 +1,46 @@
+package com.walmartlabs.concord.plugins.log;
+
+/*-
+ * *****
+ * Concord
+ * -----
+ * Copyright (C) 2017 - 2018 Walmart Inc.
+ * -----
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =====
+ */
+
+import com.walmartlabs.concord.sdk.Context;
+import com.walmartlabs.concord.sdk.Task;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Named;
+
+import static com.walmartlabs.concord.sdk.ContextUtils.assertString;
+
+@Named("logWarn")
+public class LogWarnTask implements Task {
+
+    public static final Logger log = LoggerFactory.getLogger(LogWarnTask.class);
+
+    public void call(String s) {
+        log.warn(s);
+    }
+
+    @Override
+    public void execute(Context ctx) {
+        String msg = assertString(ctx, "msg");
+        log.warn(msg);
+    }
+}
