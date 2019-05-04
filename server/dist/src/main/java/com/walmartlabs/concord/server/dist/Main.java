9b70cf061b33562c05b31a1677991df06ab9b33a
Ivan Bodrov <ibodrov@gmail.com>
2019-05-04 14:14:55 -0400

    concord-server: rename the package distr -> dist

--- server/dist/src/main/java/com/walmartlabs/concord/server/dist/Main.java
+++ server/dist/src/main/java/com/walmartlabs/concord/server/dist/Main.java
@@ -1,4 +1,4 @@
-package com.walmartlabs.concord.server;
+package com.walmartlabs.concord.server.dist;
 
 /*-
  * *****
@@ -20,6 +20,8 @@ package com.walmartlabs.concord.server;
  * =====
  */
 
+import com.walmartlabs.concord.server.ConcordServer;
+import com.walmartlabs.concord.server.Version;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.bridge.SLF4JBridgeHandler;
