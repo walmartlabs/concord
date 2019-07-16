18585aa41b11c72f47e3ffea87cb2fb84936e2df
Ivan Bodrov <IBodrov@walmartlabs.com>
2019-07-16 08:07:26 -0400

    concord-server: refactor the db configuration code (#1155)

---
+++ server/impl/src/main/java/com/walmartlabs/concord/server/cfg/InventoryDBConfiguration.java
@@ -0,0 +1,72 @@
+package com.walmartlabs.concord.server.cfg;
+
+/*-
+ * *****
+ * Concord
+ * -----
+ * Copyright (C) 2017 - 2018 Walmart Inc.
+ * -----
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =====
+ */
+
+import com.walmartlabs.concord.db.DatabaseConfiguration;
+import com.walmartlabs.concord.db.InventoryDB;
+import com.walmartlabs.ollie.config.Config;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+@Named
+@Singleton
+@InventoryDB
+public class InventoryDBConfiguration implements DatabaseConfiguration {
+
+    @Inject
+    @Config("db.url")
+    private String url;
+
+    @Inject
+    @Config("db.inventoryUsername")
+    private String username;
+
+    @Inject
+    @Config("db.inventoryPassword")
+    private String password;
+
+    @Inject
+    @Config("db.maxPoolSize")
+    private int maxPoolSize;
+
+
+    @Override
+    public String url() {
+        return url;
+    }
+
+    @Override
+    public String username() {
+        return username;
+    }
+
+    @Override
+    public String password() {
+        return password;
+    }
+
+    @Override
+    public int maxPoolSize() {
+        return maxPoolSize;
+    }
+}
