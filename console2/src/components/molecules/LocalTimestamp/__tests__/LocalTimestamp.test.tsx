8ffcc2d0e0812edf9005e702f66ee0eb350cd76e
Ivan Bodrov <IBodrov@walmartlabs.com>
2019-08-23 16:05:03 -0400

    console: update the rest of the dependencies (#1225)

--- console2/src/components/molecules/LocalTimestamp/__tests__/LocalTimestamp.test.tsx
+++ console2/src/components/molecules/LocalTimestamp/__tests__/LocalTimestamp.test.tsx
@@ -17,7 +17,7 @@
  * limitations under the License.
  * =====
  */
-import { render } from 'react-testing-library';
+import { render } from '@testing-library/react';
 import React from 'react';
 import LocalTimeStamp from '..';
 
