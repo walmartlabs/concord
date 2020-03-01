f6a94237cefec9a5e987598bb5de147fff08e5aa
Ivan Bodrov <IBodrov@walmartlabs.com>
2020-03-01 13:16:49 -0500

    examples: update dependencies (#1509)

--- examples/ansible/run.sh
+++ examples/ansible/run.sh
@@ -7,4 +7,5 @@ cp -R concord.yml playbook target/
 
 cd target && zip -r payload.zip ./* > /dev/null && cd ..
 
-curl -ikn -F archive=@target/payload.zip http://${SERVER_ADDR}/api/v1/process
+read -p "Username: " CURL_USER
+curl -u ${CURL_USER} -F archive=@target/payload.zip http://${SERVER_ADDR}/api/v1/process
