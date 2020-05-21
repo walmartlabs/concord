09dbd69be9f6741b39b760ccbca542dd92f83a4e
Benjamin Broadaway <Benjamin.Broadaway@walmart.com>
2020-05-21 13:51:54 -0500

    concord-server: update group sync ts for disabled users to clear from sync cutoff window (#1728)

--- server/impl/src/main/java/com/walmartlabs/concord/server/security/ldap/LdapGroupDao.java
+++ server/impl/src/main/java/com/walmartlabs/concord/server/security/ldap/LdapGroupDao.java
@@ -87,6 +87,10 @@ public class LdapGroupDao extends AbstractDao {
         q.execute();
     }
 
+    public void updateLastSyncTimestamp(UUID userId) {
+        tx(tx -> updateLastSyncTimestamp(tx, userId));
+    }
+
     private void updateLastSyncTimestamp(DSLContext tx, UUID userId) {
         tx.update(USERS).set(USERS.LAST_GROUP_SYNC_DT, currentTimestamp())
                 .where(USERS.USER_ID.eq(userId))
