8f4dbba2e67d882f1c20875e06969a43106ef01d
Ivan Bodrov <IBodrov@walmartlabs.com>
2020-06-09 15:34:32 -0400

    concord-console: replace redux with hooks in team list and find team dropdown (#1776)

--- console2/src/components/organisms/TeamList/index.tsx
+++ console2/src/components/organisms/TeamList/index.tsx
@@ -19,103 +19,70 @@
  */
 
 import * as React from 'react';
-import { connect } from 'react-redux';
-import { AnyAction, Dispatch } from 'redux';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { List, Loader } from 'semantic-ui-react';
 
 import { ConcordKey, RequestError } from '../../../api/common';
-import { TeamEntry } from '../../../api/org/team';
-import { actions, State } from '../../../state/data/teams';
-import { Teams } from '../../../state/data/teams';
+import { list as apiList, TeamEntry } from '../../../api/org/team';
 import { comparators } from '../../../utils';
 import { RequestErrorMessage } from '../../molecules';
 
-interface ExternalProps {
+interface Props {
     orgName: ConcordKey;
     filter?: string;
 }
 
-interface StateProps {
-    data: TeamEntry[];
-    loading: boolean;
-    error: RequestError;
-}
-
-interface DispatchProps {
-    load: (orgName: ConcordKey) => void;
-}
-
-type Props = ExternalProps & StateProps & DispatchProps;
-
-class TeamList extends React.PureComponent<Props> {
-    constructor(props: Props) {
-        super(props);
-        this.state = { onlyCurrent: true };
-    }
-
-    componentDidMount() {
-        this.update();
-    }
-
-    componentDidUpdate(prepProps: Props) {
-        if (this.props.orgName !== prepProps.orgName) {
-            this.update();
-        }
-    }
-
-    update() {
-        const { load, orgName } = this.props;
-        load(orgName);
-    }
-
-    render() {
-        const { loading, data, error } = this.props;
-
-        if (error) {
-            return <RequestErrorMessage error={error} />;
-        }
-
-        return (
-            <>
-                {loading && <Loader active={true} />}
-
-                <List divided={true} relaxed={true} size="large">
-                    {data.map((team: TeamEntry, index: number) => (
-                        <List.Item key={index}>
-                            <List.Content>
-                                <List.Header>
-                                    <Link to={`/org/${team.orgName}/team/${team.name}`}>
-                                        {team.name}
-                                    </Link>
-                                </List.Header>
-                                {team.description && (
-                                    <List.Description>{team.description}</List.Description>
-                                )}
-                            </List.Content>
-                        </List.Item>
-                    ))}
-                </List>
-            </>
-        );
-    }
-}
-
-// TODO refactor as a selector?
-const makeTeamList = (data: Teams, filter?: string): TeamEntry[] =>
-    Object.keys(data)
-        .map((k) => data[k])
+const makeTeamList = (data: TeamEntry[], filter?: string): TeamEntry[] =>
+    data
         .filter((e) => (filter ? e.name.toLowerCase().indexOf(filter.toLowerCase()) >= 0 : true))
         .sort(comparators.byName);
 
-const mapStateToProps = ({ teams }: { teams: State }, { filter }: ExternalProps): StateProps => ({
-    data: makeTeamList(teams.teamById, filter),
-    loading: teams.list.running,
-    error: teams.list.error
-});
+export default ({ orgName, filter }: Props) => {
+    const [data, setData] = useState<TeamEntry[]>([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<RequestError | undefined>();
 
-const mapDispatchToProps = (dispatch: Dispatch<AnyAction>): DispatchProps => ({
-    load: (orgName: ConcordKey) => dispatch(actions.listTeams(orgName))
-});
+    if (error) {
+        return <RequestErrorMessage error={error} />;
+    }
 
-export default connect(mapStateToProps, mapDispatchToProps)(TeamList);
+    useEffect(() => {
+        const load = async () => {
+            setLoading(true);
+            setError(undefined);
+            try {
+                setData(await apiList(orgName));
+            } catch (e) {
+                setError(e);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        load();
+    }, [orgName]);
+
+    return (
+        <>
+            {loading && <Loader active={true} />}
+
+            <List divided={true} relaxed={true} size="large">
+                {makeTeamList(data, filter).map((team: TeamEntry, index: number) => (
+                    <List.Item key={index}>
+                        <List.Content>
+                            <List.Header>
+                                <Link to={`/org/${team.orgName}/team/${team.name}`}>
+                                    {team.name}
+                                </Link>
+                            </List.Header>
+                            {team.description && (
+                                <List.Description>{team.description}</List.Description>
+                            )}
+                        </List.Content>
+                    </List.Item>
+                ))}
+            </List>
+        </>
+    );
+};
