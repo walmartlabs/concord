1b6eaa4e4c58540df7f215f196d76b053293b2e7
Yury Brigadirenko - vn0tj0b <YBrigadirenko@walmartlabs.com>
2020-06-28 21:52:48 +0300

    concord-queue-client: ignore unknown message properties, so we can extend messages without breaking backward compatibility (#1815)

--- server/queue-client/src/main/java/com/walmartlabs/concord/server/queueclient/message/Message.java
+++ server/queue-client/src/main/java/com/walmartlabs/concord/server/queueclient/message/Message.java
@@ -21,12 +21,14 @@ package com.walmartlabs.concord.server.queueclient.message;
  */
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import static com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 @JsonInclude(Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown=true)
 public abstract class Message {
 
     private final MessageType messageType;
