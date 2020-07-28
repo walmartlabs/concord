07927ce3379a0380814baf67d509e5d4361f071e
Yury Brigadirenko - vn0tj0b <YBrigadirenko@walmartlabs.com>
2020-07-29 00:38:25 +0300

    concord-server: count child processes towards the concurrency limit (#1881)

--- server/impl/src/main/java/com/walmartlabs/concord/server/process/queue/dispatcher/ConcurrentProcessFilter.java
+++ server/impl/src/main/java/com/walmartlabs/concord/server/process/queue/dispatcher/ConcurrentProcessFilter.java
@@ -67,7 +67,7 @@ public class ConcurrentProcessFilter extends WaitProcessFinishFilter {
 
     @Override
     protected List<UUID> findProcess(DSLContext tx, ProcessQueueEntry item, List<ProcessQueueEntry> startingProcesses) {
-        PolicyEngine pe = getPolicyEngine(item.orgId(), item.projectId(), item.initiatorId(), item.parentInstanceId());
+        PolicyEngine pe = getPolicyEngine(item.orgId(), item.projectId(), item.initiatorId());
         if (pe == null) {
             return Collections.emptyList();
         }
@@ -98,12 +98,8 @@ public class ConcurrentProcessFilter extends WaitProcessFinishFilter {
         return ProcessCompletionCondition.CompleteCondition.ONE_OF;
     }
 
-    private PolicyEngine getPolicyEngine(UUID orgId, UUID prjId, UUID userId, UUID parentInstanceId) {
+    private PolicyEngine getPolicyEngine(UUID orgId, UUID prjId, UUID userId) {
         if (prjId == null) {
-            return null;
-        }
-
-        if (parentInstanceId != null) {
             return null;
         }
 
