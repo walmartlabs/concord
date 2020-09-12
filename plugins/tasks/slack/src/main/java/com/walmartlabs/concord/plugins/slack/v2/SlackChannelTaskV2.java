7983ddb981124a7becb4d8e8bfe4a33e05500748
Yury Brigadirenko <YBrigadirenko@walmartlabs.com>
2020-09-12 19:09:34 +0300

    slack: runtime-v2 support (#128)

---
+++ plugins/tasks/slack/src/main/java/com/walmartlabs/concord/plugins/slack/v2/SlackChannelTaskV2.java
@@ -0,0 +1,53 @@
+package com.walmartlabs.concord.plugins.slack.v2;
+
+/*-
+ * *****
+ * Concord
+ * -----
+ * Copyright (C) 2017 - 2018 Walmart Inc.
+ * -----
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =====
+ */
+
+import com.walmartlabs.concord.plugins.slack.SlackChannelTaskCommon;
+import com.walmartlabs.concord.plugins.slack.SlackChannelTaskParams;
+import com.walmartlabs.concord.runtime.v2.sdk.Context;
+import com.walmartlabs.concord.runtime.v2.sdk.Task;
+import com.walmartlabs.concord.runtime.v2.sdk.TaskResult;
+import com.walmartlabs.concord.runtime.v2.sdk.Variables;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.util.Map;
+
+@Named("slackChannel")
+@SuppressWarnings("unused")
+public class SlackChannelTaskV2 implements Task {
+
+    private final Context context;
+
+    private final SlackChannelTaskCommon delegate = new SlackChannelTaskCommon();
+
+    @Inject
+    public SlackChannelTaskV2(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public TaskResult execute(Variables in) throws Exception {
+        Map<String, Object> result = delegate.execute(SlackChannelTaskParams.of(in, context.defaultVariables().toMap()));
+        return TaskResult.success()
+                .values(result);
+    }
+}
