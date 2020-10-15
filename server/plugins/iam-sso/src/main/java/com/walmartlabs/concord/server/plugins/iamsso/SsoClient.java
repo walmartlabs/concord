890d13c18ac5b914b343c1dea654f49c978c58f2
Daniel Cahill <daniel.cahill@ontariosystems.com>
2020-10-15 10:00:56 -0400

    SSo Client Override

--- server/plugins/iam-sso/src/main/java/com/walmartlabs/concord/server/plugins/iamsso/SsoClient.java
+++ server/plugins/iam-sso/src/main/java/com/walmartlabs/concord/server/plugins/iamsso/SsoClient.java
@@ -69,12 +69,15 @@ public class SsoClient {
         try {
             TrustManager[] trustAllCerts = new TrustManager[]{
                     new X509TrustManager() {
+                        @Override
                         public java.security.cert.X509Certificate[] getAcceptedIssuers() {
                             return null;
                         }
+                        @Override
                         public void checkClientTrusted(
                                 java.security.cert.X509Certificate[] certs, String authType) {
                         }
+                        @Overrride
                         public void checkServerTrusted(
                                 java.security.cert.X509Certificate[] certs, String authType) {
                         }
