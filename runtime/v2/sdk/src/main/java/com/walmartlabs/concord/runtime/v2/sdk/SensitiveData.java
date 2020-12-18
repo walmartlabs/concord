c7134b70fe392d3c22ccdbed7fac9faab7c139a3
Ivan Bodrov <ibodrov@gmail.com>
2020-12-17 19:07:22 -0500

    javadoc

--- runtime/v2/sdk/src/main/java/com/walmartlabs/concord/runtime/v2/sdk/SensitiveData.java
+++ runtime/v2/sdk/src/main/java/com/walmartlabs/concord/runtime/v2/sdk/SensitiveData.java
@@ -24,8 +24,23 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.ElementType.PARAMETER;
 
+/**
+ * This annotation can be used to prevent task arguments values from
+ * being recorded in process events.
+ * <p/>
+ * Currently, it is applicable only for task methods called directly
+ * via expressions. For example:
+ * <pre>{@code
+ * flows:
+ *   default:
+ *     - "${crypto.exportAsString('myOrg', 'mySecret', 'mySecretPassword')}"
+ * }</pre>
+ * Assuming the task method has the third argument annotated with
+ * {@link SensitiveData}, running this flow will result with
+ * the third argument's value being masked in process events.
+ */
 @Target({PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface SensitiveData {
