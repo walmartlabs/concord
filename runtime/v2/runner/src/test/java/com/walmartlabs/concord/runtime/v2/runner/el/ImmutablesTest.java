8ae555475ba9cb7b05fa4749f0c6b9244fa06799
Ivan Bodrov <ibodrov@gmail.com>
2021-02-03 09:58:51 -0500

    runtime-v2: add EL resolver for method accessors

---
+++ runtime/v2/runner/src/test/java/com/walmartlabs/concord/runtime/v2/runner/el/ImmutablesTest.java
@@ -0,0 +1,54 @@
+package com.walmartlabs.concord.runtime.v2.runner.el;
+
+/*-
+ * *****
+ * Concord
+ * -----
+ * Copyright (C) 2017 - 2021 Walmart Inc.
+ * -----
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =====
+ */
+
+import com.walmartlabs.concord.runtime.v2.runner.tasks.TaskProviders;
+import org.immutables.value.Value;
+import org.junit.Test;
+
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class ImmutablesTest {
+
+    @Test
+    public void test() throws Exception {
+        TestBean testBean = ImmutableTestBean.builder()
+                .foo("foo")
+                .build();
+
+        ExpressionEvaluator ee = new DefaultExpressionEvaluator(new TaskProviders());
+        Map<String, Object> vars = Collections.singletonMap("testBean", testBean);
+
+        // ---
+        String str = ee.eval(EvalContextFactory.global(new SingleFrameContext(vars)), "Hello ${testBean.foo}", String.class);
+        assertEquals("Hello foo", str);
+    }
+
+    @Value.Immutable
+    public interface TestBean extends Serializable {
+
+        String foo();
+    }
+}
