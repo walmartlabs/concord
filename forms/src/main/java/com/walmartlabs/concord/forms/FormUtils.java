d2e136681ebd9846c68499cfa6eabf22084e6bc5
Ivan Bodrov <ibodrov@gmail.com>
2021-03-02 16:55:38 -0500

    project: various minor fixes (#342)
    * concord-common: remove unused code
    * concord-common: minor code cleanup
    * examples: clean up
    * project: replace YYYY with yyyy when applicable
    * dependency-manager: add missing serialVersionUID

--- forms/src/main/java/com/walmartlabs/concord/forms/FormUtils.java
+++ forms/src/main/java/com/walmartlabs/concord/forms/FormUtils.java
@@ -46,7 +46,7 @@ public final class FormUtils {
             Constants.RUN_AS_LDAP_KEY + "." +
             Constants.RUN_AS_GROUP_KEY;
 
-    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("YYYY-MM-dd'T'HH:mm:ss.SSSZ", Locale.US);
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ", Locale.US);
 
     @SuppressWarnings("unchecked")
     public static Set<String> getRunAsUsers(String formName, Map<String, Serializable> runAsParams) {
@@ -105,7 +105,7 @@ public final class FormUtils {
      *     - group: "bGroupName"
      * </pre>
      * <p>
-     * The recommended multivalue:
+     * The recommended multi value:
      * <pre>
      * runAs:
      *   ldap:
@@ -323,6 +323,8 @@ public final class FormUtils {
     }
 
     public static class ValidationException extends Exception {
+
+        private static final long serialVersionUID = 1L;
 
         private final FormField field;
         private final String input;
