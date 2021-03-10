8350a1e0a007ceee754a53234e014c98010351de
Benjamin Broadaway <4554569+benbroadaway@users.noreply.github.com>
2021-03-10 14:03:54 -0600

    setting for older git clients

--- repository/src/main/java/com/walmartlabs/concord/repository/GitClient.java
+++ repository/src/main/java/com/walmartlabs/concord/repository/GitClient.java
@@ -135,6 +135,11 @@ public class GitClient {
                 .timeout(cfg.defaultOperationTimeout())
                 .addArgs("config", "maintenance.auto", "false")
                 .build());
+        exec(Command.builder()
+                .workDir(workDir)
+                .timeout(cfg.defaultOperationTimeout())
+                .addArgs("config", "gc.auto", "0")
+                .build());
     }
 
     private void configureRemote(Path workDir, String url) {
