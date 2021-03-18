5fba0970fe959a971fb9b656c2223cc03a3d4cc0
Ben Broadaway <4554569+benbroadaway@users.noreply.github.com>
2021-03-18 17:53:58 -0500

    concord-server: set initiator for onCancel, onFailure and onTimeout handlers (#365)

---
+++ it/server/src/test/java/com/walmartlabs/concord/it/server/TimeoutHandlingIT.java
@@ -0,0 +1,56 @@
+package com.walmartlabs.concord.it.server;
+
+/*-
+ * *****
+ * Concord
+ * -----
+ * Copyright (C) 2017 - 2021 Walmart Inc.
+ * -----
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =====
+ */
+
+import com.walmartlabs.concord.client.*;
+import org.junit.Test;
+
+import static com.walmartlabs.concord.it.common.ITUtils.archive;
+import static com.walmartlabs.concord.it.common.ServerClient.*;
+import static com.walmartlabs.concord.it.common.ServerClient.assertLog;
+
+public class TimeoutHandlingIT extends AbstractServerIT {
+    @Test(timeout = DEFAULT_TEST_TIMEOUT)
+    public void testTimeout() throws Exception {
+        ProcessApi processApi = new ProcessApi(getApiClient());
+
+        // prepare the payload
+
+        byte[] payload = archive(ProcessIT.class.getResource("timeoutHandling").toURI());
+
+        // start the process and wait for it to fail
+
+        StartProcessResponse spr = start(payload);
+
+        waitForStatus(processApi, spr.getInstanceId(), ProcessEntry.StatusEnum.TIMED_OUT);
+
+        // find the child processes
+
+        ProcessEntry child = waitForChild(processApi, spr.getInstanceId(), ProcessEntry.KindEnum.TIMEOUT_HANDLER, ProcessEntry.StatusEnum.FINISHED);
+
+        // check the handler's logs for expected messages
+
+        byte[] ab = getLog(child.getLogFileName());
+        assertLog(".*projectInfo: \\{.*orgName=Default.*\\}.*", ab);
+        assertLog(".*processInfo: \\{.*sessionKey=.*\\}.*", ab);
+        assertLog(".*initiator: \\{.*username=.*\\}.*", ab);
+    }
+}
