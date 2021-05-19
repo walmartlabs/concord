0c18a84557ea85b2217170eb357429b97677e7b2
Ben Broadaway <4554569+benbroadaway@users.noreply.github.com>
2021-05-19 11:07:08 -0500

    smtp-tasks: read default params in runtime-v2 (#419)

--- plugins/tasks/smtp/src/main/java/com/walmartlabs/concord/plugins/smtp/SmtpTaskV2.java
+++ plugins/tasks/smtp/src/main/java/com/walmartlabs/concord/plugins/smtp/SmtpTaskV2.java
@@ -27,7 +27,6 @@ import com.walmartlabs.concord.runtime.v2.sdk.Variables;
 
 import javax.inject.Inject;
 import javax.inject.Named;
-import java.io.Serializable;
 import java.nio.file.Path;
 import java.util.Collections;
 import java.util.Map;
@@ -72,6 +71,10 @@ public class SmtpTaskV2 implements Task {
         Map<String, Object> m = input.getMap(s, null);
         if (m == null) {
             m = ctx.variables().getMap(s, null);
+        }
+
+        if (m == null) {
+            m = ctx.defaultVariables().getMap(s, null);
         }
         return m;
     }
