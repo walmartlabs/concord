bd40b8b4e5394c4ac4d94b093867bd96647986e1
Benjamin Broadaway <4554569+benbroadaway@users.noreply.github.com>
2021-07-28 08:58:56 -0500

    persist with saveRetryFile argument

--- examples/ansible_retry/run.sh
+++ examples/ansible_retry/run.sh
@@ -9,4 +9,4 @@ cp -R concord.yml playbook target/
 cd target && zip -r payload.zip ./* > /dev/null && cd ..
 
 read -p "Username: " CURL_USER
-curl -u ${CURL_USER} -F archive=@target/payload.zip http://${SERVER_ADDR}/api/v1/process
+curl -u "${CURL_USER}" -F entryPoint="${ENTRY_POINT}" -F archive=@target/payload.zip "http://${SERVER_ADDR}/api/v1/process"
