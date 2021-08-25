a450867fe43d52200cba1cd202842626e40a0283
Brigadirenko Yury <ybrigo@gmail.com>
2021-08-26 00:08:57 +0300

    up

--- examples/custom_task/src/main/java/com/walmartlabs/concord/examples/customtask/CustomTask.java
+++ examples/custom_task/src/main/java/com/walmartlabs/concord/examples/customtask/CustomTask.java
@@ -1,24 +1,69 @@
 package com.walmartlabs.concord.examples.customtask;
 
-import com.walmartlabs.concord.runtime.v2.sdk.Context;
-import com.walmartlabs.concord.runtime.v2.sdk.Task;
-import com.walmartlabs.concord.runtime.v2.sdk.TaskResult;
-import com.walmartlabs.concord.runtime.v2.sdk.Variables;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.walmartlabs.concord.sdk.Context;
+import com.walmartlabs.concord.sdk.ContextUtils;
+import com.walmartlabs.concord.sdk.Task;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.HashMap;
+import java.util.Map;
 
-@Named("reposolns")
+/**
+ * A custom task example. All tasks must implement {@link Task} interface and
+ * be marked with {@link Named} annotation.
+ *
+ * The task fetches a specified URL and parses the response as JSON, converting
+ * the data into regular Java objects.
+ */
+@Named("customTask")
 public class CustomTask implements Task {
 
-    @Inject
-    public CustomTask(Context context) {
-        System.out.println("ctor");
-    }
+    private static final Logger log = LoggerFactory.getLogger(CustomTask.class);
 
     @Override
-    public TaskResult execute(Variables input) {
-        System.out.println("execute: " + input.toMap());
-        return TaskResult.success();
+    public void execute(Context ctx) throws Exception {
+        // retrieve a variable using the helper class
+        String url = ContextUtils.assertString(ctx, "url");
+
+
+        OkHttpClient client = new OkHttpClient();
+        Request req = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response resp = client.newCall(req).execute()) {
+            if (!resp.isSuccessful()) {
+                log.warn("Error while fetching {}: {}", url, resp.code());
+                ctx.setVariable("result", error(resp.code()));
+                return;
+            }
+
+            ObjectMapper om = new ObjectMapper();
+            Object data = om.readValue(resp.body().byteStream(), Object.class);
+
+            // do not use custom classes and data structures when setting variables in a task
+            // stick to the default JDK classes to avoid serialization issues
+            ctx.setVariable("result", ok(data));
+        }
+    }
+
+    private static Object ok(Object data) {
+        Map<String, Object> m = new HashMap<>();
+        m.put("ok", true);
+        m.put("data", data);
+        return m;
+    }
+
+    private static Object error(int code) {
+        Map<String, Object> m = new HashMap<>();
+        m.put("ok", false);
+        m.put("errorCode", code);
+        return m;
     }
 }
