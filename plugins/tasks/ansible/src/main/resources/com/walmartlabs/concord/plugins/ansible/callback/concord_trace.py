270c1c3c7717ee6fe30ebaab235572b81efdffa6
Brigadirenko Yury <ybrigo@gmail.com>
2022-03-28 17:04:38 +0300

    ansible: stats file for flow with multiple playbooks

--- plugins/tasks/ansible/src/main/resources/com/walmartlabs/concord/plugins/ansible/callback/concord_trace.py
+++ plugins/tasks/ansible/src/main/resources/com/walmartlabs/concord/plugins/ansible/callback/concord_trace.py
@@ -30,7 +30,7 @@ class CallbackModule(CallbackBase):
         self.base_dir = os.environ['_CONCORD_ATTACHMENTS_DIR']
 
     def log(self, data):
-        target_dir = self.base_dir;
+        target_dir = self.base_dir
         mkdir_p(target_dir)
 
         target_filename = target_dir + "/ansible_stats.json"
@@ -38,6 +38,25 @@ class CallbackModule(CallbackBase):
         target_file.write(json.dumps(data, indent=2))
 
         print("Trace saved to:", target_filename)
+
+        entry = {'playbook': self.playbook._file_name, 'stats': data}
+
+        target_filename = target_dir + "/ansible_stats_v2.json"
+        if not os.path.isfile(target_filename):
+            with open(target_filename, mode='w') as f:
+                f.write(json.dumps([entry], indent=2))
+        else:
+            with open(target_filename) as f:
+                stats = json.load(f)
+
+            stats.append(entry)
+            with open(target_filename, mode='w') as f:
+                f.write(json.dumps(stats, indent=2))
+
+        print("Trace saved to:", target_filename)
+
+    def v2_playbook_on_start(self, playbook):
+        self.playbook = playbook
 
     def playbook_on_stats(self, stats):
         self.log(ConcordAnsibleStats.build_stats_data(stats))
