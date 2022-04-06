8f402eb3346ade6ceb96efa0f13174000dde4459
Ivan Bodrov <ibodrov@gmail.com>
2022-04-06 08:50:07 -0400

    project: improve mvnd support (#567)

--- server/plugins/ansible/impl/src/main/java/com/walmartlabs/concord/server/plugins/ansible/ProcessEventEntry.java
+++ server/plugins/ansible/impl/src/main/java/com/walmartlabs/concord/server/plugins/ansible/ProcessEventEntry.java
@@ -25,6 +25,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonRawValue;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import io.swagger.annotations.ApiModel;
 import org.immutables.value.Value;
 
 import javax.annotation.Nullable;
@@ -32,6 +33,7 @@ import java.io.Serializable;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+@ApiModel(value="AnsibleProcessEventEntry")
 @Value.Immutable
 @Value.Style(jdkOnly = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
