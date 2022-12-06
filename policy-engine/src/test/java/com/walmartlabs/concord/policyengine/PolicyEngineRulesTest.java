003e6740772a0aae74cc4cccb417421a9718ee1e
Yury Brigadirenko <ybrigo@gmail.com>
2022-12-06 17:36:51 +0100

    policy-engine: cron trigger policy (#686)

--- policy-engine/src/test/java/com/walmartlabs/concord/policyengine/PolicyEngineRulesTest.java
+++ policy-engine/src/test/java/com/walmartlabs/concord/policyengine/PolicyEngineRulesTest.java
@@ -209,6 +209,7 @@ public class PolicyEngineRulesTest {
                 .rawPayloadRule(rawPayloadRule)
                 .runtimeRule(runtimeRule)
                 .putCustomRule("ansible", Collections.singletonMap("k", "v"))
+                .cronTriggerRule(CronTriggerRule.of("msg", 1000))
                 .build();
 
         String s = om.writeValueAsString(r);
