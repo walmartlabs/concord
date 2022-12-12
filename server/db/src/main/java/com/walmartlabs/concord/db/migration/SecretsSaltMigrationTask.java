700dd37b818b25ac0a9c28692601ebb917efc047
Ginni Hema Sundara Rao <hemant0328@gmail.com>
2022-12-12 12:15:39 +0530

    Added migration task to update secrets data with new SHA algorithm

--- server/db/src/main/java/com/walmartlabs/concord/db/migration/SecretsSaltMigrationTask.java
+++ server/db/src/main/java/com/walmartlabs/concord/db/migration/SecretsSaltMigrationTask.java
@@ -44,7 +44,7 @@ public class SecretsSaltMigrationTask implements MigrationTask {
     private byte[] secretSalt;
 
     @Override
-    public void execute(DataSource dataSource) {
+    synchronized public void execute(DataSource dataSource) {
         log.info("Starting migration task for updating secret salt");
         int retryCount = 0;
         while (retryCount <= MAX_RETRIES) {
