6bbc39bd618779154ef79c1cd88cd153374c25b9
Ginni Hema Sundara Rao <hemant0328@gmail.com>
2022-12-21 18:32:19 +0530

    Formatted and removed retry mechanism

--- server/liquibase-ext/src/main/java/com/walmartlabs/concord/server/liquibase/ext/migration/SecretsHashMigrationTask.java
+++ server/liquibase-ext/src/main/java/com/walmartlabs/concord/server/liquibase/ext/migration/SecretsHashMigrationTask.java
@@ -9,9 +9,9 @@ package com.walmartlabs.concord.server.liquibase.ext.migration;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -57,54 +57,48 @@ public class SecretsHashMigrationTask implements CustomTaskChange {
     @Override
     public void execute(Database database) throws CustomChangeException {
         log.info("Starting migration task for secretsHash");
-        int retryCount = 0;
-        while (retryCount <= MAX_RETRIES) {
-            try {
-                JdbcConnection con = (JdbcConnection) database.getConnection();
-                byte[] serverPwd = serverPassword.getBytes(StandardCharsets.UTF_8);
-                con.setAutoCommit(false);
-                while (true) {
-                    try (PreparedStatement psSelect = con.prepareStatement("SELECT secret_id, secret_salt, secret_data FROM secrets WHERE hash_algorithm = ? AND encrypted_by = ? LIMIT ? FOR UPDATE")) {
-                        psSelect.setString(1, HashAlgorithm.LEGACY_MD5.getName());
-                        psSelect.setString(2, SecretEncryptedByType.SERVER_KEY.toString());
-                        psSelect.setInt(3, BATCH_SIZE);
-                        ResultSet resultSet = psSelect.executeQuery();
-                        int count = 0;
-                        try (PreparedStatement psUpdated = con.prepareStatement("UPDATE secrets SET secret_data = ?, hash_algorithm = ? WHERE secret_id = ?::uuid")) {
-                            while (resultSet.next()) {
-                                String secretId = resultSet.getString(1);
-                                byte[] secretSalt = resultSet.getBytes(2);
-                                byte[] encryptedData = resultSet.getBytes(3);
-                                byte[] decryptedData = SecretUtils.decrypt(encryptedData, serverPwd, secretSalt, HashAlgorithm.LEGACY_MD5);
-                                byte[] newlyEncryptedData = SecretUtils.encrypt(decryptedData, serverPwd, secretSalt, HashAlgorithm.SHA256);
-                                psUpdated.setBytes(1, newlyEncryptedData);
-                                psUpdated.setString(2, HashAlgorithm.SHA256.getName());
-                                psUpdated.setObject(3, secretId);
-                                psUpdated.addBatch();
-                                count++;
-                            }
-                            if (count > 0) {
-                                psUpdated.executeBatch();
-                                con.commit();
-                            }
-                            log.info("Committing {} records with new hash algorithm", count);
-                            if (count < BATCH_SIZE) {
-                                break;
-                            }
+        try {
+            JdbcConnection con = (JdbcConnection) database.getConnection();
+            byte[] serverPwd = serverPassword.getBytes(StandardCharsets.UTF_8);
+            con.setAutoCommit(false);
+            while (true) {
+                try (PreparedStatement psSelect = con.prepareStatement("SELECT secret_id, secret_salt, secret_data FROM secrets WHERE hash_algorithm = ? AND encrypted_by = ? LIMIT ? FOR UPDATE")) {
+                    psSelect.setString(1, HashAlgorithm.LEGACY_MD5.getName());
+                    psSelect.setString(2, SecretEncryptedByType.SERVER_KEY.toString());
+                    psSelect.setInt(3, BATCH_SIZE);
+                    ResultSet resultSet = psSelect.executeQuery();
+                    int count = 0;
+                    try (PreparedStatement psUpdated = con.prepareStatement("UPDATE secrets SET secret_data = ?, hash_algorithm = ? WHERE secret_id = ?::uuid")) {
+                        while (resultSet.next()) {
+                            String secretId = resultSet.getString(1);
+                            byte[] secretSalt = resultSet.getBytes(2);
+                            byte[] encryptedData = resultSet.getBytes(3);
+                            byte[] decryptedData = SecretUtils.decrypt(encryptedData, serverPwd, secretSalt, HashAlgorithm.LEGACY_MD5);
+                            byte[] newlyEncryptedData = SecretUtils.encrypt(decryptedData, serverPwd, secretSalt, HashAlgorithm.SHA256);
+                            psUpdated.setBytes(1, newlyEncryptedData);
+                            psUpdated.setString(2, HashAlgorithm.SHA256.getName());
+                            psUpdated.setObject(3, secretId);
+                            psUpdated.addBatch();
+                            count++;
                         }
-                        resultSet.close();
+                        if (count > 0) {
+                            psUpdated.executeBatch();
+                            con.commit();
+                        }
+                        log.info("Committing {} records with new hash algorithm", count);
+                        if (count < BATCH_SIZE) {
+                            break;
+                        }
                     }
+                    resultSet.close();
                 }
-                log.info("Successfully completed migrating password less secrets with SHA256 hash algorithm");
-                break;
-            } catch (Exception e) {
-                if (retryCount == MAX_RETRIES) {
-                    log.error("secret hash migration -> Error while running secret hash migration. giving up");
-                    throw new RuntimeException(e);
-                }
-                log.error("Exception: {} while executing migration task for {} time.", e.getMessage(), ++retryCount);
             }
+        } catch (Exception e) {
+            log.error("Exception in executing secretsHashMigrationTask, message : {}", e.getMessage());
+            throw new CustomChangeException(e);
         }
+        log.info("Successfully completed migrating password less secrets with SHA256 hash algorithm");
+
     }
 
     @Override
