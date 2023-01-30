8d6b53d7f52dded7fb369d8a0deeef0ab53e3c89
cdaringe <cdaringe@github.com>
2023-01-30 15:12:10 -0800

    feat: add Variables::withMap

--- runtime/v2/sdk/src/main/java/com/walmartlabs/concord/runtime/v2/sdk/Variables.java
+++ runtime/v2/sdk/src/main/java/com/walmartlabs/concord/runtime/v2/sdk/Variables.java
@@ -9,9 +9,9 @@ package com.walmartlabs.concord.runtime.v2.sdk;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,6 +28,13 @@ import java.util.UUID;
 public interface Variables {
 
     Object get(String key);
+
+    default Variables withMap(Map<String, Object> map) {
+      for(Map.Entry<String, Object> entry: map.entrySet()) {
+        set(entry.getKey(), entry.getValue());
+      }
+      return this;
+    }
 
     void set(String key, Object value);
 
