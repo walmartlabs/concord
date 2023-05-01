d9ce7b4409864e01ae67d557134e45684141b261
brig <ybrigo@gmail.com>
2023-05-01 16:13:00 +0200

    project: fix JDK 17 Sisu compatibility issues

--- server/plugins/ansible/db/src/main/java/com/walmartlabs/concord/server/plugins/ansible/db/DatabaseModule.java
+++ server/plugins/ansible/db/src/main/java/com/walmartlabs/concord/server/plugins/ansible/db/DatabaseModule.java
@@ -1,17 +1,17 @@
-package com.walmartlabs.concord.runtime.v2.parser;
+package com.walmartlabs.concord.server.plugins.ansible.db;
 
 /*-
  * *****
  * Concord
  * -----
- * Copyright (C) 2017 - 2019 Walmart Inc.
+ * Copyright (C) 2017 - 2018 Walmart Inc.
  * -----
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,20 +20,19 @@ package com.walmartlabs.concord.runtime.v2.parser;
  * =====
  */
 
-import org.immutables.serial.Serial;
-import org.immutables.value.Value;
+import com.google.inject.Binder;
+import com.google.inject.Module;
+import com.walmartlabs.concord.db.DatabaseChangeLogProvider;
 
-import java.io.Serializable;
-import java.util.Map;
+import javax.inject.Named;
 
-@Value.Immutable
-@Value.Style(jdkOnly = true)
-@Serial.Version(-8269676324702677451L)
-public interface SimpleOptions extends StepOptions {
+import static com.google.inject.multibindings.Multibinder.newSetBinder;
 
-    static SimpleOptions of(Map<String, Serializable> meta) {
-        return ImmutableSimpleOptions.builder()
-                .meta(meta)
-                .build();
+@Named
+public class DatabaseModule implements Module {
+
+    @Override
+    public void configure(Binder binder) {
+        newSetBinder(binder, DatabaseChangeLogProvider.class).addBinding().to(AnsibleDBChangeLogProvider.class);
     }
 }
