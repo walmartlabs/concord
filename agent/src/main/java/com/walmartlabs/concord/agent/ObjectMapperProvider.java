254b3b5cf2477722314f1e82dd837ba3143cbe83
Ivan Bodrov <ibodrov@gmail.com>
2023-11-21 10:14:51 -0500

    concord-agent: bind classes explicitly

--- agent/src/main/java/com/walmartlabs/concord/agent/ObjectMapperProvider.java
+++ agent/src/main/java/com/walmartlabs/concord/agent/ObjectMapperProvider.java
@@ -20,18 +20,34 @@ package com.walmartlabs.concord.agent;
  * =====
  */
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.guava.GuavaModule;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
-import javax.inject.Named;
 import javax.inject.Provider;
-import javax.inject.Singleton;
 
-@Named
-@Singleton
 public class ObjectMapperProvider implements Provider<ObjectMapper> {
 
     @Override
     public ObjectMapper get() {
-        return new ObjectMapper();
+        ObjectMapper mapper = new ObjectMapper()
+                .registerModule(new Jdk8Module())
+                .registerModule(new GuavaModule())
+                .registerModule(new JavaTimeModule());
+
+        // Write dates as ISO-8601
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        // Ignore unknown properties
+        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+
+        // Ignore nulls
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+        return mapper;
     }
 }
