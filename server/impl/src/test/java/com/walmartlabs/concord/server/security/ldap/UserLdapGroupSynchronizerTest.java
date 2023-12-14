d233a49559c2e5985d2f7a8280f0e3671322970e
Daniel Kleveros <daniel.kleveros@walmart.com>
2023-12-14 14:25:42 -0800

    Refactoring

--- server/impl/src/test/java/com/walmartlabs/concord/server/security/ldap/UserLdapGroupSynchronizerTest.java
+++ server/impl/src/test/java/com/walmartlabs/concord/server/security/ldap/UserLdapGroupSynchronizerTest.java
@@ -4,7 +4,6 @@ import com.walmartlabs.concord.server.cfg.LdapGroupSyncConfiguration;
 import com.walmartlabs.concord.server.user.UserManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import scala.Tuple2;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,26 +26,6 @@ class UserLdapGroupSynchronizerTest {
     }
 
     @Test
-    void checkOrgOwnerDiscrepanciesDiffUsersOnlyInLdap() {
-        UserLdapGroupSynchronizer.UserItem user1 = new UserLdapGroupSynchronizer.UserItem(UUID.randomUUID(), "admin", "domain", false);
-        UserLdapGroupSynchronizer.UserItem user2 = new UserLdapGroupSynchronizer.UserItem(UUID.randomUUID(), "testUser", null, false);
-
-        List<UserLdapGroupSynchronizer.UserItem> ldapGroupUsers = new ArrayList<>();
-        ldapGroupUsers.add(user1);
-        ldapGroupUsers.add(user2);
-
-        List<UserLdapGroupSynchronizer.UserItem> ownerRoleUsers = new ArrayList<>();
-        ownerRoleUsers.add(user1);
-
-        List<UserLdapGroupSynchronizer.UserItem> usersOnlyInLdapGroup = new ArrayList<>();
-        usersOnlyInLdapGroup.add(user2);
-
-        Tuple2<List, List> values = userLdapGroupSynchronizer.checkOrgOwnerDiscrepancies(ldapGroupUsers, ownerRoleUsers);
-        assertTrue(values._1().equals(usersOnlyInLdapGroup));
-        assertTrue(values._2().isEmpty());
-    }
-
-    @Test
     void checkOrgOwnerDiscrepanciesDiffOwnersNotInLdap() {
         UserLdapGroupSynchronizer.UserItem user1 = new UserLdapGroupSynchronizer.UserItem(UUID.randomUUID(), "admin", "domain", false);
         UserLdapGroupSynchronizer.UserItem user2 = new UserLdapGroupSynchronizer.UserItem(UUID.randomUUID(), "testUser", null, false);
@@ -61,9 +40,7 @@ class UserLdapGroupSynchronizerTest {
         List<UserLdapGroupSynchronizer.UserItem> ownersNotInLdapGroup = new ArrayList<>();
         ownersNotInLdapGroup.add(user2);
 
-        Tuple2<List, List> values = userLdapGroupSynchronizer.checkOrgOwnerDiscrepancies(ldapGroupUsers, ownerRoleUsers);
-        assertTrue(values._1().isEmpty());
-        assertTrue(values._2().equals(ownersNotInLdapGroup));
+        assertTrue(userLdapGroupSynchronizer.checkOrgOwnerDiscrepancies(ldapGroupUsers, ownerRoleUsers).equals(ownersNotInLdapGroup));
     }
 
     @Test
@@ -79,8 +56,6 @@ class UserLdapGroupSynchronizerTest {
         ownerRoleUsers.add(user1);
         ownerRoleUsers.add(user2);
 
-        Tuple2<List, List> values = userLdapGroupSynchronizer.checkOrgOwnerDiscrepancies(ldapGroupUsers, ownerRoleUsers);
-        assertTrue(values._1().isEmpty());
-        assertTrue(values._2().isEmpty());
+        assertTrue(userLdapGroupSynchronizer.checkOrgOwnerDiscrepancies(ldapGroupUsers, ownerRoleUsers).isEmpty());
     }
 }
