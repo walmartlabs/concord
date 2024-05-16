8a2282d40442a02df930f0192f4dd1f2b5e6e391
brig <ybrigo@gmail.com>
2024-05-16 01:35:39 -0400

    server: process wait conditions in batch mode

--- server/impl/src/main/java/com/walmartlabs/concord/server/process/waits/WaitProcessFinishHandler.java
+++ server/impl/src/main/java/com/walmartlabs/concord/server/process/waits/WaitProcessFinishHandler.java
@@ -20,17 +20,27 @@ package com.walmartlabs.concord.server.process.waits;
  * =====
  */
 
+import com.codahale.metrics.Histogram;
+import com.codahale.metrics.MetricRegistry;
+import com.walmartlabs.concord.db.AbstractDao;
+
+import com.walmartlabs.concord.server.cfg.ProcessWaitWatchdogConfiguration;
+import com.walmartlabs.concord.server.jooq.tables.ProcessQueue;
+
 import com.walmartlabs.concord.db.MainDB;
 import com.walmartlabs.concord.server.process.queue.ProcessQueueManager;
-import com.walmartlabs.concord.server.sdk.ProcessKey;
 import com.walmartlabs.concord.server.sdk.ProcessStatus;
 import com.walmartlabs.concord.server.sdk.metrics.WithTimer;
 import org.jooq.Configuration;
+import org.jooq.impl.EnumConverter;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static com.walmartlabs.concord.server.jooq.tables.ProcessQueue.PROCESS_QUEUE;
+import static com.walmartlabs.concord.server.process.waits.ProcessCompletionCondition.CompleteCondition;
 
 /**
  * Handles the processes that are waiting for other processes to finish.
@@ -38,11 +48,24 @@ import java.util.UUID;
 @Singleton
 public class WaitProcessFinishHandler implements ProcessWaitHandler<ProcessCompletionCondition> {
 
+    private final Dao dao;
+
+    private final int processLimitForStatusQuery;
+
     private final ProcessQueueManager processQueueManager;
 
+    private final Histogram waitProcessesHistogram;
+
+
     @Inject
-    public WaitProcessFinishHandler(ProcessQueueManager processQueueManager) {
+    public WaitProcessFinishHandler(Dao dao,
+                                    ProcessWaitWatchdogConfiguration cfg,
+                                    ProcessQueueManager processQueueManager,
+                                    MetricRegistry metricRegistry) {
+        this.dao = dao;
+        this.processLimitForStatusQuery = cfg.getProcessLimitForStatusQuery();
         this.processQueueManager = processQueueManager;
+        this.waitProcessesHistogram = metricRegistry.histogram("wait-process-finish-histogram");
     }
 
     @Override
@@ -52,16 +75,127 @@ public class WaitProcessFinishHandler implements ProcessWaitHandler<ProcessCompl
 
     @Override
     @WithTimer
-    public Result<ProcessCompletionCondition> process(ProcessKey processKey, ProcessCompletionCondition wait) {
-        Set<UUID> awaitProcesses = wait.processes();
-        if (!awaitProcesses.isEmpty()) {
-            return Result.of(wait);
+    public List<Result<ProcessCompletionCondition>> processBatch(List<WaitConditionItem<ProcessCompletionCondition>> items) {
+        Set<UUID> allProcesses = items.stream()
+                .flatMap(i -> i.waitCondition().processes().stream())
+                .collect(Collectors.toSet());
+
+        waitProcessesHistogram.update(allProcesses.size());
+
+        List<WaitConditionItem<ProcessCompletionCondition>> modifiableItems = new ArrayList<>();
+        for (WaitConditionItem<ProcessCompletionCondition> item : items) {
+            modifiableItems.add(toModifiable(item));
         }
 
-        if (wait.resumeEvent() != null) {
-            return Result.resume(wait.resumeEvent());
-        } else {
-            return Result.action(tx -> processQueueManager.updateExpectedStatus(tx, processKey, ProcessStatus.WAITING, ProcessStatus.ENQUEUED));
+        List<Set<UUID>> processesBatch = split(allProcesses, processLimitForStatusQuery);
+        List<Result<ProcessCompletionCondition>> results = new ArrayList<>();
+        for (Set<UUID> processes : processesBatch) {
+            Map<UUID, ProcessStatus> statuses = dao.findStatuses(processes);
+
+            for (var it = modifiableItems.iterator(); it.hasNext(); ) {
+                WaitConditionItem<ProcessCompletionCondition> item = it.next();
+
+                filterFinished(item.waitCondition(), statuses);
+                if (item.waitCondition().processes().isEmpty()) {
+                    results.add(toResult(item));
+                    it.remove();
+                }
+            }
+
+            if (modifiableItems.isEmpty()) {
+                break;
+            }
+        }
+
+        for (WaitConditionItem<ProcessCompletionCondition> item : modifiableItems) {
+            results.add(toResult(item));
+        }
+
+        return results;
+    }
+
+    private Result<ProcessCompletionCondition> toResult(WaitConditionItem<ProcessCompletionCondition> item) {
+        Set<UUID> newAwaitProcesses = item.waitCondition().processes();
+        if (newAwaitProcesses.isEmpty()) {
+            if (item.waitCondition().resumeEvent() != null) {
+                return Result.resume(item, item.waitCondition().resumeEvent());
+            } else {
+                return Result.action(item, tx -> processQueueManager.updateExpectedStatus(tx, item.processKey(), ProcessStatus.WAITING, ProcessStatus.ENQUEUED));
+            }
+        }
+
+        return Result.of(item.processKey(), item.waitConditionId(),
+                ProcessCompletionCondition.builder().from(item.waitCondition())
+                        .processes(newAwaitProcesses)
+                        .build());
+    }
+
+    private static WaitConditionItem<ProcessCompletionCondition> toModifiable(WaitConditionItem<ProcessCompletionCondition> item) {
+        return WaitConditionItem.of(item.processKey(), item.waitConditionId(), ModifiableProcessCompletionCondition.create().from(item.waitCondition()));
+    }
+
+    private static List<Set<UUID>> split(Set<UUID> processes, int maxProcessesItems) {
+        List<Set<UUID>> subsets = new ArrayList<>();
+        Set<UUID> currentSet = new HashSet<>();
+
+        for (UUID element : processes) {
+            currentSet.add(element);
+            if (currentSet.size() >= maxProcessesItems) {
+                subsets.add(new HashSet<>(currentSet));
+                currentSet.clear();
+            }
+        }
+
+        if (!currentSet.isEmpty()) {
+            subsets.add(currentSet);
+        }
+
+        return subsets;
+    }
+
+    private static void filterFinished(ProcessCompletionCondition condition, Map<UUID, ProcessStatus> statuses) {
+        if (condition.processes() == null || condition.processes().isEmpty()) {
+            return;
+        }
+
+        Iterator<UUID> it = condition.processes().iterator();
+        while (it.hasNext()) {
+            UUID proc = it.next();
+            if (statuses.containsKey(proc)) {
+                ProcessStatus status = statuses.get(proc);
+                if (status == null || condition.finalStatuses().contains(status)) {
+                    if (condition.completeCondition() == CompleteCondition.ALL) {
+                        it.remove();
+                    } else if (condition.completeCondition() == CompleteCondition.ONE_OF) {
+                        condition.processes().clear();
+                        return;
+                    } else {
+                        throw new RuntimeException("Unknown wait complete condition: " + condition.completeCondition());
+                    }
+                }
+            }
+        }
+    }
+
+    public static class Dao extends AbstractDao {
+
+        @Inject
+        private Dao(@MainDB Configuration cfg) {
+            super(cfg);
+        }
+
+        public Map<UUID, ProcessStatus> findStatuses(Set<UUID> processes) {
+            Map<UUID, ProcessStatus> result = txResult(tx -> {
+                ProcessQueue q = PROCESS_QUEUE.as("q");
+                return tx.select(q.INSTANCE_ID, q.CURRENT_STATUS)
+                        .from(q)
+                        .where(q.INSTANCE_ID.in(processes))
+                        .fetchMap(q.INSTANCE_ID, r -> r.get(q.CURRENT_STATUS, new EnumConverter<>(String.class, ProcessStatus.class)));
+            });
+            for (UUID key : processes) {
+                result.putIfAbsent(key, null);
+            }
+            return result;
         }
     }
 }
