9af06d8770682bd1357e0eebb85d33c3d06c0b31
Ivan Bodrov <ibodrov@gmail.com>
2024-06-05 19:44:57 -0400

    project: update Liquibase to 4.8.0 (#912)

---
+++ server/db/src/main/java/com/walmartlabs/concord/db/LiquibaseLogService.java
@@ -0,0 +1,53 @@
+package com.walmartlabs.concord.db;
+
+/*-
+ * *****
+ * Concord
+ * -----
+ * Copyright (C) 2017 - 2024 Walmart Inc.
+ * -----
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =====
+ */
+
+import liquibase.logging.LogMessageFilter;
+import liquibase.logging.LogService;
+import liquibase.logging.Logger;
+
+public class LiquibaseLogService implements LogService {
+
+    @Override
+    public int getPriority() {
+        return PRIORITY_SPECIALIZED;
+    }
+
+    @Override
+    public Logger getLog(Class clazz) {
+        return new LiquibaseLogger();
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public LogMessageFilter getFilter() {
+        return null;
+    }
+
+    @Override
+    public void setFilter(LogMessageFilter filter) {
+
+    }
+}
