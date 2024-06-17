57d18fcff047330b03f4f34210610fb2361b15f7
brig <ybrigo@gmail.com>
2024-06-16 21:46:29 -0400

    cleanup

--- examples/runtime-v2/mocks/run-tests.sh
+++ examples/runtime-v2/mocks/run-tests.sh
@@ -7,6 +7,5 @@ cp -R main.concord.yaml tests target/
 
 cd target && zip -r payload.zip ./* > /dev/null && cd ..
 
-#read -p "Username: " CURL_USER
-#curl -u ${CURL_USER}
-curl -u myuser:q1 -F entryPoint=allTests -F concord.yaml=@tests-runner.concord.yaml -F archive=@target/payload.zip http://${SERVER_ADDR}/api/v1/process
+read -p "Username: " CURL_USER
+curl -u ${CURL_USER} -F entryPoint=allTests -F concord.yaml=@tests-runner.concord.yaml -F archive=@target/payload.zip http://${SERVER_ADDR}/api/v1/process
