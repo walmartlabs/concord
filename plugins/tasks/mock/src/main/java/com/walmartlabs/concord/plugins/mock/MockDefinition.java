e7d1ee27653361ca01a82df939207ccce4e03c04
Yury Brigadirenko <ybrigo@gmail.com>
2024-06-17 17:54:11 +0200

    mocks: examples, stored input, add throwError (#928)

--- plugins/tasks/mock/src/main/java/com/walmartlabs/concord/plugins/mock/MockDefinition.java
+++ plugins/tasks/mock/src/main/java/com/walmartlabs/concord/plugins/mock/MockDefinition.java
@@ -24,6 +24,7 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.immutables.value.Value;
 
+import javax.annotation.Nullable;
 import java.util.Collections;
 import java.util.Map;
 
@@ -55,4 +56,10 @@ public interface MockDefinition {
     default Map<String, Object> out() {
         return Collections.emptyMap();
     }
+
+    @Nullable
+    String inputStoreId();
+
+    @Nullable
+    String throwError();
 }
