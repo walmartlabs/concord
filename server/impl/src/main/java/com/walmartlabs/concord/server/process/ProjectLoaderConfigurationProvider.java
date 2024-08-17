a8d970b187fed50e09284a66e15d973c594b5b28
Ivan Bodrov <ibodrov@gmail.com>
2024-08-17 10:21:42 -0400

    concord-server: allow non-standard runtimes
    Add `process.extraRuntimes` configuration parameter to
    concord-server.conf. Only the default v1, v2 and `extraRuntimes` will be
    allowed as `configuration.runtime` values.

---
+++ server/impl/src/main/java/com/walmartlabs/concord/server/process/ProjectLoaderConfigurationProvider.java
@@ -0,0 +1,46 @@
+package com.walmartlabs.concord.server.process;
+
+/*-
+ * *****
+ * Concord
+ * -----
+ * Copyright (C) 2017 - 2024 Walmart Inc.
+ * -----
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =====
+ */
+
+import com.walmartlabs.concord.process.loader.ProjectLoader.ProjectLoaderConfiguration;
+import com.walmartlabs.concord.server.cfg.ProcessConfiguration;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Provider;
+import java.util.Optional;
+import java.util.Set;
+
+@Named
+public class ProjectLoaderConfigurationProvider implements Provider<ProjectLoaderConfiguration> {
+
+    private final Set<String> extraRuntimes;
+
+    @Inject
+    public ProjectLoaderConfigurationProvider(ProcessConfiguration cfg) {
+        this.extraRuntimes = Optional.ofNullable(cfg.getExtraRuntimes()).map(Set::copyOf).orElse(Set.of());
+    }
+
+    @Override
+    public ProjectLoaderConfiguration get() {
+        return new ProjectLoaderConfiguration(Set.copyOf(extraRuntimes));
+    }
+}
