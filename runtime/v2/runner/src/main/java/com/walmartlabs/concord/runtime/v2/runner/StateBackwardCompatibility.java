f8d839a6fa8a3d3ae76ddeacfd25b13c712667ee
Ivan Bodrov <ibodrov@gmail.com>
2024-10-16 08:51:05 -0400

    comments, license header

--- runtime/v2/runner/src/main/java/com/walmartlabs/concord/runtime/v2/runner/StateBackwardCompatibility.java
+++ runtime/v2/runner/src/main/java/com/walmartlabs/concord/runtime/v2/runner/StateBackwardCompatibility.java
@@ -1,11 +1,34 @@
 package com.walmartlabs.concord.runtime.v2.runner;
 
+/*-
+ * *****
+ * Concord
+ * -----
+ * Copyright (C) 2017 - 2024 Walmart Inc.
+ * -----
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =====
+ */
+
 import com.walmartlabs.concord.runtime.v2.model.*;
 
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Backward compatibility for reading old {@link ProcessSnapshot} instances.
+ */
 public final class StateBackwardCompatibility {
 
     public static ProcessSnapshot apply(ProcessSnapshot processSnapshot) {
@@ -13,7 +36,10 @@ public final class StateBackwardCompatibility {
             return null;
         }
 
-        // ProcessDefinition.flows type change:
+        // ProcessDefinition.flows and ProcessDefinition.profiles.flows has changed
+        // from Map<String, List<Step>> to Map<String, Flow>.
+        // Here we convert the old format to the new one.
+
         ProcessDefinition processDefinition = processSnapshot.processDefinition();
         if (isOldFlowsDefinition(processDefinition.flows())) {
             return ProcessSnapshot.builder().from(processSnapshot)
