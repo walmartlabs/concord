e093e94451528e8b270ad21c727710ebdde51e70
brig <ybrigo@gmail.com>
2024-10-25 14:03:17 -0400

    mocks permanent mode

--- plugins/tasks/mock/src/main/java/com/walmartlabs/concord/plugins/mock/MockDefinition.java
+++ plugins/tasks/mock/src/main/java/com/walmartlabs/concord/plugins/mock/MockDefinition.java
@@ -9,9 +9,9 @@ package com.walmartlabs.concord.plugins.mock;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -31,6 +31,7 @@ import java.util.Map;
 /**
     mocks:
       - name: "http"
+        permanent: true
         in:
           url: ".*"
           method: "POST"
@@ -55,6 +56,11 @@ public interface MockDefinition {
     @Value.Default
     default Map<String, Object> out() {
         return Collections.emptyMap();
+    }
+
+    @Value.Default
+    default boolean permanent() {
+        return false;
     }
 
     @Nullable
