d1cfd77b152204ad4a77d8944dc9fb28b5e62843
brig <ybrigo@gmail.com>
2024-10-28 20:17:03 -0400

    junit extension

--- plugins/tasks/mock/src/test/java/com/walmartlabs/concord/plugins/mock/MainTest.java
+++ plugins/tasks/mock/src/test/java/com/walmartlabs/concord/plugins/mock/MainTest.java
@@ -1,17 +1,42 @@
 package com.walmartlabs.concord.plugins.mock;
 
-import com.walmartlabs.concord.runtime.v2.runner.AbstractTest;
-import org.junit.jupiter.api.Test;
+/*-
+ * *****
+ * Concord
+ * -----
+ * Copyright (C) 2017 - 2024 Walmart Inc.
+ * -----
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =====
+ */
 
-public class MainTest extends AbstractTest {
+import com.walmartlabs.concord.runtime.v2.runner.TestRuntimeV2;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import static com.walmartlabs.concord.runtime.v2.runner.TestRuntimeV2.assertLog;
+
+public class MainTest {
+
+    @RegisterExtension
+    private static final TestRuntimeV2 runtime = new TestRuntimeV2();
 
     @Test
-    public void test() throws Exception {
-        deploy("simple");
+    public void testTaskMock() throws Exception {
+        runtime.deploy("simple");
 
-        save(cfgFromDeployment().build());
-
-        byte[] log = run();
+        byte[] log = runtime.run();
+        assertLog(log, ".*The actual task is not being executed; this is a mock.*");
         assertLog(log, ".*result.ok: true.*");
     }
 }
