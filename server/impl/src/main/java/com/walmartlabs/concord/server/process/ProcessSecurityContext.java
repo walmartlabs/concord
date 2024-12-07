c7d824dcc0d2b1ed6a620e91ca6da44b43a6d8ef
Ivan Bodrov <ibodrov@gmail.com>
2024-12-06 21:32:17 -0500

    concord-server: invalidate principal cache when state is updated

--- server/impl/src/main/java/com/walmartlabs/concord/server/process/ProcessSecurityContext.java
+++ server/impl/src/main/java/com/walmartlabs/concord/server/process/ProcessSecurityContext.java
@@ -80,16 +80,15 @@ public class ProcessSecurityContext {
         return SecurityUtils.serialize(dst);
     }
 
-    // TODO: invalidate cache for processKey?
     public void storeCurrentSubject(ProcessKey processKey) {
         Subject s = SecurityUtils.getSubject();
         PrincipalCollection src = s.getPrincipals();
         storeSubject(processKey, src);
     }
 
-    // TODO: invalidate cache for processKey?
     public void storeSubject(ProcessKey processKey, PrincipalCollection src) {
         stateManager.replace(processKey, PRINCIPAL_FILE_PATH, serializePrincipals(src));
+        principalCache.invalidate(processKey);
     }
 
     public PrincipalCollection getPrincipals(PartialProcessKey processKey) {
