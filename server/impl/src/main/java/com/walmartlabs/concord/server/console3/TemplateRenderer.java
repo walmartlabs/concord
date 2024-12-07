4a4ff289a004d7fb36639e2cfc54420137fd7e5e
Ivan Bodrov <ibodrov@gmail.com>
2024-12-06 21:08:24 -0500

    wip

--- server/impl/src/main/java/com/walmartlabs/concord/server/console3/TemplateRenderer.java
+++ server/impl/src/main/java/com/walmartlabs/concord/server/console3/TemplateRenderer.java
@@ -9,9 +9,9 @@ package com.walmartlabs.concord.server.console3;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,15 +22,14 @@ package com.walmartlabs.concord.server.console3;
 
 import org.thymeleaf.ITemplateEngine;
 import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.TemplateSpec;
 import org.thymeleaf.context.IContext;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.util.Set;
 
-@SuppressWarnings("OptionalUsedAsFieldOrParameterType")
 public class TemplateRenderer {
 
     private final ITemplateEngine engine;
@@ -49,12 +48,11 @@ public class TemplateRenderer {
         this.engine = engine;
     }
 
-    public void render(String resource,
-                       Set<String> templateSelectors,
+    public void render(TemplateSpec templateSpec,
                        IContext context,
                        OutputStream out) {
 
         var writer = new OutputStreamWriter(out);
-        engine.process(resource, templateSelectors, context, writer);
+        engine.process(templateSpec, context, writer);
     }
 }
