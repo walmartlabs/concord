527f13e5d6053b5234817ac697162f3be5889f07
Ivan Bodrov <ibodrov@gmail.com>
2024-12-13 15:18:20 -0500

    wip

--- server/impl/src/main/java/com/walmartlabs/concord/server/console3/TemplateResponseWriter.java
+++ server/impl/src/main/java/com/walmartlabs/concord/server/console3/TemplateResponseWriter.java
@@ -18,6 +18,7 @@ import javax.ws.rs.ext.Provider;
 import java.io.OutputStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
+import java.util.Map;
 import java.util.Set;
 
 import static com.walmartlabs.concord.server.console3.ConsoleModule.BASE_PATH;
@@ -44,12 +45,13 @@ public class TemplateResponseWriter implements MessageBodyWriter<TemplateRespons
     public void writeTo(TemplateResponse templateResponse, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws WebApplicationException {
         var templateSelectors = request.getHeader("HX-Request") != null ? Set.of("content") : Set.<String>of(); // in case of HTMX requests, render only the "content" part of the template
         var templateSpec = new TemplateSpec(templateResponse.template(), templateSelectors, TemplateMode.HTML, null);
-        var context = prepareContext(request, response);
+        var context = prepareContext(request, response, templateResponse.extraVars());
         templateRenderer.render(templateSpec, context, entityStream);
     }
 
     private static WebContext prepareContext(HttpServletRequest request,
-                                             HttpServletResponse response) {
+                                             HttpServletResponse response,
+                                             Map<String, Object> extraVars) {
 
         var app = ThymeleafApp.getInstance(request);
         var ctx = new WebContext(app.buildExchange(request, response));
@@ -59,6 +61,8 @@ public class TemplateResponseWriter implements MessageBodyWriter<TemplateRespons
 
         var principal = UserPrincipal.getCurrent();
         ctx.setVariable("user", principal != null ? principal.getUser() : null);
+
+        ctx.setVariables(extraVars);
 
         return ctx;
     }
