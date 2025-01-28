0fdf4e0193a76b345869668e7a99888d4f035cfe
Ivan Bodrov <ibodrov@gmail.com>
2025-01-28 13:23:23 -0500

    concord-agent-operator: use informers API (#1072)

--- agent-operator/src/main/java/com/walmartlabs/concord/agentoperator/Operator.java
+++ agent-operator/src/main/java/com/walmartlabs/concord/agentoperator/Operator.java
@@ -25,67 +25,74 @@ import com.walmartlabs.concord.agentoperator.agent.AgentClientFactory;
 import com.walmartlabs.concord.agentoperator.crd.AgentPool;
 import com.walmartlabs.concord.agentoperator.crd.AgentPoolList;
 import com.walmartlabs.concord.agentoperator.scheduler.AutoScalerFactory;
-import com.walmartlabs.concord.agentoperator.scheduler.Event;
 import com.walmartlabs.concord.agentoperator.scheduler.Scheduler;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
-import io.fabric8.kubernetes.client.Watcher;
-import io.fabric8.kubernetes.client.WatcherException;
+import io.fabric8.kubernetes.client.informers.ResourceEventHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.concurrent.Executors;
+
+import static com.walmartlabs.concord.agentoperator.scheduler.Event.Type.DELETED;
+import static com.walmartlabs.concord.agentoperator.scheduler.Event.Type.MODIFIED;
 
 public class Operator {
 
     private static final Logger log = LoggerFactory.getLogger(Operator.class);
 
+    private static final long RESYNC_PERIOD = Duration.ofSeconds(10).toMillis();
+
     public static void main(String[] args) {
         // TODO support overloading the CRD with an external file?
 
-        var namespace = System.getenv("WATCH_NAMESPACE");
-        if (namespace == null) {
-            namespace = "default";
-        }
-
-        var client = new DefaultKubernetesClient() // NOSONAR
-                .inNamespace(namespace);
-
+        var namespace = getEnv("WATCH_NAMESPACE", "default");
         var baseUrl = getEnv("CONCORD_BASE_URL", "http://192.168.99.1:8001"); // use minikube/vbox host's default address
         var apiToken = getEnv("CONCORD_API_TOKEN", null);
         var useMaintenanceMode = Boolean.parseBoolean(getEnv("USE_AGENT_MAINTENANCE_MODE", "false"));
 
         // TODO use secrets for the token?
         var cfg = new Scheduler.Configuration(baseUrl, apiToken);
+        var client = new DefaultKubernetesClient().inNamespace(namespace);
         var autoScalerFactory = new AutoScalerFactory(cfg, client);
         var agentClientFactory = new AgentClientFactory(useMaintenanceMode);
+        var executor = Executors.newCachedThreadPool();
+
         var scheduler = new Scheduler(autoScalerFactory, client, agentClientFactory);
+        var handler = new ResourceEventHandler<AgentPool>() {
+            @Override
+            public void onAdd(AgentPool resource) {
+                executor.submit(() -> scheduler.onEvent(MODIFIED, resource));
+            }
+
+            @Override
+            public void onUpdate(AgentPool oldResource, AgentPool newResource) {
+                if (oldResource == newResource) {
+                    return;
+                }
+                executor.submit(() -> scheduler.onEvent(MODIFIED, newResource));
+            }
+
+            @Override
+            public void onDelete(AgentPool resource, boolean deletedFinalStateUnknown) {
+                executor.submit(() -> scheduler.onEvent(DELETED, resource));
+            }
+        };
+
+        var informer = client.resources(AgentPool.class, AgentPoolList.class)
+                .inAnyNamespace()
+                .inform(handler, RESYNC_PERIOD);
+
         scheduler.start();
 
-        log.info("main -> my watch begins... (namespace={})", namespace);
-
-        var dummyClient = client.resources(AgentPool.class, AgentPoolList.class);
         try {
-            dummyClient.watch(new Watcher<>() {
-                @Override
-                public void eventReceived(Action action, AgentPool resource) {
-                    scheduler.onEvent(actionToEvent(action), resource);
-                }
-
-                @Override
-                public void onClose(WatcherException we) {
-                    log.error("Watcher exception  {}", we.getMessage(), we);
-                }
-            });
+            informer.run();
         } catch (Exception e) {
             log.error("Error while watching for CRs (namespace={})", namespace, e);
             System.exit(2);
         }
-    }
 
-    private static Event.Type actionToEvent(Watcher.Action action) {
-        return switch (action) {
-            case ADDED, MODIFIED -> Event.Type.MODIFIED;
-            case DELETED -> Event.Type.DELETED;
-            default -> throw new IllegalArgumentException("Unknown action type: " + action);
-        };
+        log.info("main -> and so my watch begins... (namespace={})", namespace);
     }
 
     private static String getEnv(String key, String defaultValue) {
