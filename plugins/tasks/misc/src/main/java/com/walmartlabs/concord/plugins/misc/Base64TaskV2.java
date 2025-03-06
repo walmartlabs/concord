868233e0d6f41cc428e3480090119b3da3ad0857
Yury Brigadirenko <ybrigo@gmail.com>
2025-03-05 21:58:55 -0500

    plugins: new env and collections tasks (#1092)

--- plugins/tasks/misc/src/main/java/com/walmartlabs/concord/plugins/misc/Base64TaskV2.java
+++ plugins/tasks/misc/src/main/java/com/walmartlabs/concord/plugins/misc/Base64TaskV2.java
@@ -9,9 +9,9 @@ package com.walmartlabs.concord.plugins.misc;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,6 +20,7 @@ package com.walmartlabs.concord.plugins.misc;
  * =====
  */
 
+import com.walmartlabs.concord.runtime.v2.sdk.DryRunReady;
 import com.walmartlabs.concord.runtime.v2.sdk.Task;
 
 import javax.inject.Named;
@@ -28,6 +29,7 @@ import java.util.Base64;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 @Named("base64")
+@DryRunReady
 public class Base64TaskV2 implements Task {
 
     /**
