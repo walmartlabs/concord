02f97a8cdf6675fcdd86dd2939cf4cc68b7b6dac
brig <ybrigo@gmail.com>
2025-06-12 14:36:25 -0400

    runtime-v2: support expressions for output variables in call step

--- runtime/v2/model/src/main/java/com/walmartlabs/concord/runtime/v2/model/FlowCallOptions.java
+++ runtime/v2/model/src/main/java/com/walmartlabs/concord/runtime/v2/model/FlowCallOptions.java
@@ -9,9 +9,9 @@ package com.walmartlabs.concord.runtime.v2.model;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -50,6 +50,16 @@ public interface FlowCallOptions extends StepOptions {
         return Collections.emptyList();
     }
 
+    @Nullable
+    String outExpression();
+
+    @Value.Default
+    @AllowNulls
+    default Map<String, Serializable> outMapping() {
+        return Collections.emptyMap();
+    }
+
+    @Deprecated(forRemoval = true, since = "2.28.0")
     @Value.Default
     @AllowNulls
     default Map<String, Serializable> outExpr() {
--- runtime/v2/model/src/main/java/com/walmartlabs/concord/runtime/v2/serializer/FlowCallStepSerializer.java
+++ runtime/v2/model/src/main/java/com/walmartlabs/concord/runtime/v2/serializer/FlowCallStepSerializer.java
@@ -9,9 +9,9 @@ package com.walmartlabs.concord.runtime.v2.serializer;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -64,6 +64,8 @@ public class FlowCallStepSerializer extends StdSerializer<FlowCall> {
         writeNotEmptyObjectField("in", options.inputExpression(), gen);
 
         writeNotEmptyObjectField("out", options.out(), gen);
+        writeNotEmptyObjectField("out", options.outExpression(), gen);
+        writeNotEmptyObjectField("out", options.outMapping(), gen);
         writeNotEmptyObjectField("out", options.outExpr(), gen);
 
         if (options.withItems() != null) {
