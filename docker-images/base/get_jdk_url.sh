75328879e3f779bad407291a8f648ff00ad47435
benbroadaway <4554569+benbroadaway@users.noreply.github.com>
2026-03-08 17:03:28 -0500

    docker-images: JDK25 build

--- docker-images/base/get_jdk_url.sh
+++ docker-images/base/get_jdk_url.sh
@@ -6,6 +6,8 @@ JDK_17_AMD64="https://github.com/adoptium/temurin17-binaries/releases/download/j
 JDK_17_ARM64="https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.15%2B6/OpenJDK17U-jdk_aarch64_linux_hotspot_17.0.15_6.tar.gz"
 JDK_21_AMD64="https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.7%2B6/OpenJDK21U-jdk_x64_linux_hotspot_21.0.7_6.tar.gz"
 JDK_21_ARM64="https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.7%2B6/OpenJDK21U-jdk_aarch64_linux_hotspot_21.0.7_6.tar.gz"
+JDK_25_AMD64="https://github.com/adoptium/temurin25-binaries/releases/download/jdk-25.0.2%2B10/OpenJDK25U-jdk_x64_linux_hotspot_25.0.2_10.tar.gz"
+JDK_25_ARM64="https://github.com/adoptium/temurin25-binaries/releases/download/jdk-25.0.2%2B10/OpenJDK25U-jdk_aarch64_linux_hotspot_25.0.2_10.tar.gz"
 
 case "${JDK_VERSION}-${TARGETARCH}" in
   "17-amd64")
@@ -19,6 +21,12 @@ case "${JDK_VERSION}-${TARGETARCH}" in
     ;;
   "21-arm64")
     echo ${JDK_21_ARM64}
+    ;;
+  "25-amd64")
+    echo ${JDK_25_AMD64}
+    ;;
+  "25-arm64")
+    echo ${JDK_25_ARM64}
     ;;
   *)
     >&2 echo "Unsupported JDK ${JDK_VERSION}-${TARGETARCH}"
